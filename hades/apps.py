1011171a3ecaba61718c378034ff426f5d4f3f71
argon <xamit.94@gmail.com>
2016-08-27 18:18:54 +0530

    initial commit. Has a few models

---
+++ hades/apps.py
@@ -0,0 +1,7 @@
+from __future__ import unicode_literals
+
+from django.apps import AppConfig
+
+
+class HadesConfig(AppConfig):
+    name = 'hades'
---
+++ poseidon/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def index(request):
+    return HttpResponse("Welcome Shopper. Complete your profile infomation to be eligible for audit")
+
+def profile(request):
+    return HttpResponse("Shopper profile")
+
+def details(request):
+    return HttpResponse("Shopper details page")
+
+def submit(request):
+    return HttpResponse("View on submitting response")
