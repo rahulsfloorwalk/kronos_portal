1e1203b8a27bd901076162e456fb975b8a66a6a3
argon <xamit.94@gmail.com>
2016-08-31 22:27:36 +0530

    adds views and urls to auditor

--- auditor/urls.py
+++ auditor/urls.py
@@ -4,4 +4,8 @@ from . import views
 urlpatterns = ([
 # urls must be of form /<user_id>/dashboard
     url(r'dashboard', views.dashboard, name="dashboard"),
+    url(r'profile$', views.profile, name="profile"),
+    url(r'profile/edit', views.profile_edit, name="profile_edit"),
+    url(r'profile/additional/edit', views.profile_additional_edit, name="profile_additional_edit"),
+    url(r'profile/bank/edit', views.profile_bank_edit, name="profile_bank_edit"),
 ], 'auditor')
--- auditor/views.py
+++ auditor/views.py
@@ -1,14 +1,23 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
 
+@login_required
 def dashboard(request):
     return HttpResponse("Welcome Shopper. Complete your profile infomation to be eligible for audit")
 
+@login_required
 def profile(request):
     return HttpResponse("Shopper profile")
 
-def details(request):
-    return HttpResponse("Shopper details page")
+@login_required
+def profile_edit(request):
+    return HttpResponse("Shopper edit details page")
 
-def submit(request):
-    return HttpResponse("View on submitting response")
+@login_required
+def profile_additional_edit(request):
+    return HttpResponse("Shopper additional info edit page")
+
+@login_required
+def profile_bank_edit(request):
+    return HttpResponse("Shopper additional info edit page")
