dff1fe8b72b618d465620aa53a0fe0de275654ac
argon <xamit.94@gmail.com>
2016-10-15 03:37:18 +0530

    adds setializers and views for all manager resources

--- client/serializers.py
+++ client/serializers.py
@@ -1,7 +1,7 @@
 from rest_framework import routers, viewsets
 from rest_framework.serializers import ModelSerializer, ValidationError
 
-from .models import City, Location, Client, Audit, AuditLocation
+from .models import City, Location, Client, Audit, AuditLocation, City
 
 class ClientSerializer(ModelSerializer):
     class Meta:
@@ -16,9 +16,75 @@ class ClientSerializer(ModelSerializer):
 
     def save(self, **kwargs):
         client = Client()
-        client.name = kwargs['data']['name']
-        client.email = kwargs['data']['email']
-        client.phone = kwargs['data']['phone']
+        client.name = self.validated_data.get('name')
+        client.email = self.validated_data.get('email')
+        client.phone = self.validated_data.get('phone')
 
         client.save()
         return client
+
+class LocationSerializer(ModelSerializer):
+    class Meta:
+        model = Location
+        fields = (
+            'id',
+            'name',
+            'pincode',
+            'city_id',
+        )
+        read_only_fields = ('id',)
+
+    def save(self, **kwargs):
+        location = Location()
+        location.name = self.validated_data.get('name')
+        location.pincode = self.validated_data.get('pincode')
+        location.city_id = kwargs['city']
+
+        location.save()
+        return location
+
+class AuditSerializer(ModelSerializer):
+    class Meta:
+        model = Audit
+        fields = (
+            'id',
+            'type',
+            'status',
+            'start_date',
+            'end_date',
+            'description',
+            'client_id',
+        )
+        read_only_fields = ('id',)
+
+    def save(self, **kwargs):
+        audit = Audit()
+        audit.type = self.validated_data.get('type')
+        audit.status = self.validated_data.get('status')
+        audit.start_date = self.validated_data.get('start_date')
+        audit.end_date = self.validated_data.get('end_date')
+        audit.description = self.validated_data.get('description')
+        audit.client_id = kwargs['client']
+
+        audit.save()
+        return audit
+
+class AuditLocationSerializer(ModelSerializer):
+    class Meta:
+        model = AuditLocation
+        fields = (
+            'id',
+            'audit_id',
+            'location_id',
+            'count',
+        )
+        read_only_fields = ('id',)
+
+    def save(self, **kwargs):
+        audit_location = AuditLocation()
+        audit_location.count = self.validated_data.get('count')
+        audit_location.audit_id = kwargs['audit']
+        audit_location.location_id = kwargs['location']
+
+        audit_location.save()
+        return audit_location
--- client/views.py
+++ client/views.py
@@ -6,14 +6,13 @@ from django.utils.decorators import method_decorator
 from rest_framework.views import APIView
 from rest_framework.response import Response
 
-from .models import Client, Audit, Location, AuditLocation
-from .serializers import ClientSerializer
+from .models import Client, Audit, Location, AuditLocation, City
+from .serializers import ClientSerializer, LocationSerializer, AuditSerializer, AuditLocationSerializer
 
 class ClientView(APIView):
     def get(self, request, format=None):
         try:
             client = Client.objects.all()
-            print(client)
             return Response(ClientSerializer(client, many=True).data)
         except Client.DoesNotExist:
             raise Http404
@@ -21,17 +20,48 @@ class ClientView(APIView):
     def post(self, request):
         client_s = ClientSerializer(data=request.data)
         client_s.is_valid(raise_exception=True)
-        client = client_s.save(data=request.data)
+        client = client_s.save()
         return Response(ClientSerializer(client).data)
 
 class LocationView(APIView):
     def get(self, request, format=None):
-        return None
+        try:
+            location = Location.objects.all()
+            return Response(LocationSerializer(location, many=True).data)
+        except Location.DoesNotExist:
+            return Http404
+
+    def post(self, request):
+        location_s = LocationSerializer(data=request.data)
+        location_s.is_valid(raise_exception=True)
+        location = location_s.save(city=request.data['city_id'])
+        return Response(LocationSerializer(location).data)
 
 class AuditView(APIView):
     def get(self, request, format=None):
-        return None
+        try:
+            audit = Audit.objects.all()
+            return Response(AuditSerializer(audit, many=True).data)
+        except Audit.DoesNotExist:
+            return Http404
+
+    def post(self, request):
+        audit_s = AuditSerializer(data=request.data)
+        audit_s.is_valid(raise_exception=True)
+        audit = audit_s.save(client=request.data['client_id'])
+        return Response(AuditSerializer(audit).data)
 
 class AuditLocationView(APIView):
     def get(self, request, format=None):
-        return None
+        try:
+            audit_location = AuditLocation.objects.all()
+            return Response(AuditLocationSerializer(audit_location, many=True).data)
+        except AuditLocation.DoesNotExist:
+            return Http404
+
+    def post(self, request):
+        audit_location_s = AuditLocationSerializer(data=request.data)
+        audit_location_s.is_valid(raise_exception=True)
+        audit_location = audit_location_s.save(audit=request.data['audit_id'], location=request.data['location_id'])
+        return Response(AuditLocationSerializer(audit_location).data)
+
