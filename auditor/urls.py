97a0e0c8f12fcc0fc3f390ad512dfe03fda811e5
satyanash <satyajeet@webexports.co.in>
2016-11-26 21:11:16 +0530

    remove old UI views, urls and templates from auditor app

--- auditor/urls.py
+++ auditor/urls.py
@@ -2,19 +2,12 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = ([
-    url(r'dashboard', views.dashboard, name="dashboard"),
-
     url(r'state$', views.StateView.as_view(), name='state_view'),
     url(r'city/(?P<state>[\w\-]+)$', views.CityView.as_view(), name='city_view'),
 
     url(r'profile-api$', views.ProfileInfoView.as_view(), name="profile_info_view"),
     url(r'additional-api$', views.AdditionalInfoView.as_view(), name="additional_info_view"),
     url(r'bank-api$', views.BankInfoView.as_view(), name="bank_info_view"),
-
-    url(r'profile$', views.profile, name="profile"),
-    url(r'profile/edit', views.ProfileInfoFormView.as_view(), name="profile_info_edit"),
-    url(r'profile/additional/edit', views.AdditionalInfoFormView.as_view(), name="additional_info_edit"),
-    url(r'profile/bank/edit', views.BankInfoFormView.as_view(), name="bank_info_edit"),
 
     url(r'audit$', views.AvailableAuditsView.as_view(), name="available_audits"),
     url(r'audit/(?P<audit_id>[0-9]+)/location/(?P<location_id>[0-9]+)/application/cancel$', views.AuditApplicationCancelView.as_view(), name="audit_application_cancel_view"),
