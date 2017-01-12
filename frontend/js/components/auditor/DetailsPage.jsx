e5fb790d63a531165e1e46795fc2e57449ce698d
satyanash <satyajeet@webexports.co.in>
2017-01-12 22:03:18 +0530

    remove Zoho vote message

--- frontend/js/components/auditor/DetailsPage.jsx
+++ frontend/js/components/auditor/DetailsPage.jsx
@@ -13,14 +13,6 @@ var DetailsPage = React.createClass({
 		return (
 			<div className="row">
 				<div className="col-md-6">
-					<Panel type="primary" title="Vote for FloorWalk">
-						<p>FloorWalk has been selected in Zoho's Small Businesses SELL BIG with ZOHO Contest</p>
-						<p>Cast your vote for FloorWalk and show your appreciation.</p>
-						<p><b>#SmallBizSellBig</b></p>
-						<a target="_blank" href="https://www.zoho.com/sellbig/vote/e2b86301a1d2a8bef74faf0c0302816890b61cfc" className="btn btn-primary btn-lg">
-							Vote For Us
-						</a>
-					</Panel>
 					<ProfileInfoPanel/>
 					<Panel title="Password" body={true}>
 						<p><a href="/auth/password_change">Click here</a> to change your password.</p>
