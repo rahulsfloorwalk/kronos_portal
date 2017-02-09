536fdff0a6811a1a44302beafbaf0297535922c2
Kaustubh Gadkari <k2gadkari87@gmail.com>
2017-02-09 17:35:55 +0530

    add table/model reportsection in answer

--- answer/models.py
+++ answer/models.py
@@ -3,6 +3,7 @@ from django.db.models import Model, CharField, IntegerField, AutoField, DateFiel
 from django.db.models import CASCADE
 from questionnaire.models import Question
 from audit_store.models import AuditStore
+from questionnaire.models import Section
 
 class Answer(Model):
 
@@ -13,3 +14,17 @@ class Answer(Model):
 
     answer_text = CharField(db_column='answer_text', max_length=2048, blank=True)
     marks_obtained = IntegerField(db_column='marks_obtained', blank=True, null=True)
+    
+class ReportSection(Model):
+    
+    id = AutoField(db_column = 'id', primary_key=True)
+    audit_store = ForeignKey('audit_store.AuditStore', db_column='audit_store_id', blank=False)
+    section = ForeignKey('questionnaire.Section', db_column='section_id', blank=False)
+    pm_comment = CharField(db_column='pm_comment', max_length=2048, blank=True)
+    auditor_comment = CharField(db_column='auditor_comment', max_length=2048, blank=True)
+    
+    def __str__(self):
+        return "ReportSection({}): {}, {}".format(self.id, self.pm_comment, self.auditor_comment)
+
+    class Meta:
+        unique_together = (("audit_store","section"))    
