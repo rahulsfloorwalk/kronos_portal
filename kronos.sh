2cfcf546eecf3d40cc6213f0f3005bc1f8f38055
satyanash <satyajeet@webexports.co.in>
2017-03-07 14:54:30 +0530

    daemonize gunicorn

--- kronos.sh
+++ kronos.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-gunicorn --error-logfile ./logs/gunicorn.err.log --access-logfile ./logs/gunicorn.access.log --log-level debug --bind 0.0.0.0:8000 -p ./logs/gunicorn.pid kronos.wsgi
+gunicorn --error-logfile ./logs/gunicorn.err.log --access-logfile ./logs/gunicorn.access.log --log-level debug --bind 0.0.0.0:8000 -p ./logs/gunicorn.pid -D kronos.wsgi
