82d7918d607f76d0d0d26198676af5c664b793a9
satyanash <satyajeet@webexports.co.in>
2017-04-07 21:25:10 +0530

    push pending answer.js

---
+++ frontend/js/manager/service/answer.js
@@ -0,0 +1,14 @@
+import $ from 'jquery'
+import { url } from '../../../config.js'
+
+export function setAnswerText(audit_store_id, question_id, answer_text){
+	return $.ajax({
+		url: url.api_base_path + `manager/audit_store/${audit_store_id}/question/${question_id}/answer_text`,
+		method: 'POST',
+		data: JSON.stringify({
+			answer_text
+		}),
+		contentType: 'application/json'
+	});
+};
+
