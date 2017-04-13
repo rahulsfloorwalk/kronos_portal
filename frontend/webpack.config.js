de4d143c2f6ada370e666d7fef1595e9b9cb0628
satyanash <satyajeet@webexports.co.in>
2017-04-14 00:38:22 +0530

    separate vendor and app js files for client, auditor and manager

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -5,8 +5,14 @@ var ExtractTextPlugin = require("extract-text-webpack-plugin");
 module.exports = {
 	entry: {
 		auditor: path.resolve(__dirname, './js/auditor.jsx'),
+		auditor_vendor: ['jquery','react','react-dom','react-redux','redux','redux-thunk','redux-logger','react-router','react-ga'],
+
 		manager: path.resolve(__dirname, './js/manager.jsx'),
+		manager_vendor: ['jquery','react','react-dom','react-redux','redux','redux-thunk','redux-logger','react-router'],
+
 		client: path.resolve(__dirname, './client/client.jsx'),
+		client_vendor: ['jquery','react','react-dom','react-router'],
+
 		'react-datetime': path.resolve(__dirname, './node_modules/react-datetime/css/react-datetime.css'),
 	},
 	output: {
@@ -40,6 +46,9 @@ module.exports = {
 		//		NODE_ENV: JSON.stringify("production")
 		//	}
 		//}),
+		new webpack.optimize.CommonsChunkPlugin({ name: "auditor_vendor", chunks: ['auditor']}),
+		new webpack.optimize.CommonsChunkPlugin({ name: "client_vendor", chunks: ['client']}),
+		new webpack.optimize.CommonsChunkPlugin({ name: "manager_vendor", chunks: ['manager']}),
 		new ExtractTextPlugin("[name].css")
 	]
 };
