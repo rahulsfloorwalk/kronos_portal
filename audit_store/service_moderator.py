cab5a2e2c53a5d51177e3a7acfbf2e8961b31441
satyanash <satyajeet@webexports.co.in>
2017-04-26 19:04:33 +0530

    reuse AuditStore transactional logic

--- audit_store/service_moderator.py
+++ audit_store/service_moderator.py
@@ -11,6 +11,7 @@ from registration.service.moderator import find_moderator_by_user_id
 
 from .models import AuditStore
 import audit.service.audit_cycle as audit_cycle_service
+from . import service as audit_store_service
 
 
 def find_by_audit_cycle_for_moderator(audit_cycle_id, user_id):
@@ -32,150 +33,35 @@ def find_by_id_for_moderator(audit_store_id, user_id):
 
 @atomic
 def complete_for_moderator(audit_store_id, user_id):
-    try:
-        user = find_moderator_by_user_id(user_id)
-        audit_store = find_by_id_for_moderator(audit_store_id, user_id)
-
-        if audit_store.status == AuditStore.SUBMITTED:
-            audit_store.status = AuditStore.COMPLETED
-            audit_store.save()
-            #TODO:VERB should be encapsulated
-            notify.send(
-                user,
-                recipient=Group.objects.get(name=GROUP_NAME_MANAGER),
-                verb='AUDIT_STORE_COMPLETED',
-                action_object=audit_store,
-                target=audit_store.audit
-            )
-            notify.send(
-                user,
-                recipient=audit_store.user,
-                verb='AUDIT_STORE_COMPLETED',
-                action_object=audit_store,
-                target=audit_store.audit
-            )
-            return audit_store
-        else:
-            raise AppLogicError("audit store cannot be completed now")
-    except AuditStore.DoesNotExist as e:
-        raise ObjectNotFound from e
+    user = find_moderator_by_user_id(user_id)
+    audit_store = find_by_id_for_moderator(audit_store_id, user_id)
+    return audit_store_service.complete(audit_store.id, user)
 
 
 @atomic
 def fail_for_moderator(audit_store_id, user_id):
-    try:
-        user = find_moderator_by_user_id(user_id)
-        audit_store = find_by_id_for_moderator(audit_store_id, user_id)
+    user = find_moderator_by_user_id(user_id)
+    audit_store = find_by_id_for_moderator(audit_store_id, user_id)
+    return audit_store_service.fail(audit_store.id, user)
 
-        if audit_store.status in (AuditStore.SUBMITTED, AuditStore.ASSIGNED):
-            audit_store.status = AuditStore.FAILED
-            audit_store.save()
-            #TODO:VERB should be encapsulated
-            notify.send(
-                user,
-                recipient=Group.objects.get(name=GROUP_NAME_MANAGER),
-                verb='AUDIT_STORE_FAILED',
-                action_object=audit_store,
-                target=audit_store.audit
-            )
-            notify.send(
-                user,
-                recipient=audit_store.user,
-                verb='AUDIT_STORE_FAILED',
-                action_object=audit_store,
-                target=audit_store.audit
-            )
-            return audit_store
-        else:
-            raise AppLogicError("audit store cannot be failed now")
-    except AuditStore.DoesNotExist as e:
-        raise ObjectNotFound from e
 
 @atomic
 def submit_for_moderator(audit_store_id, user_id):
-    try:
-        user = find_moderator_by_user_id(user_id)
-        audit_store = find_by_id_for_moderator(audit_store_id, user_id)
-
-        if audit_store.status == AuditStore.ASSIGNED:
-            audit_store.status = AuditStore.SUBMITTED
-            audit_store.save()
-            #TODO:VERB should be encapsulated
-            notify.send(
-                user,
-                recipient=Group.objects.get(name=GROUP_NAME_MANAGER),
-                verb='AUDIT_STORE_SUBMITTED',
-                action_object=audit_store,
-                target=audit_store.audit
-            )
-            notify.send(
-                user,
-                recipient=audit_store.user,
-                verb='AUDIT_STORE_SUBMITTED',
-                action_object=audit_store,
-                target=audit_store.audit
-            )
-            return audit_store
-        else:
-            raise AppLogicError("audit store cannot be unsubmitted now")
-    except AuditStore.DoesNotExist as e:
-        raise ObjectNotFound from e
+    user = find_moderator_by_user_id(user_id)
+    audit_store = find_by_id_for_moderator(audit_store_id, user_id)
+    return audit_store_service.submit_by_manager(audit_store.id, user)
 
 
 @atomic
 def set_audit_date_for_moderator(audit_store_id, audit_date, user_id):
-    try:
-        user = find_moderator_by_user_id(user_id)
-        audit_store = find_by_id_for_moderator(audit_store_id, user_id)
+    user = find_moderator_by_user_id(user_id)
+    audit_store = find_by_id_for_moderator(audit_store_id, user_id)
+    return audit_store_service.set_audit_date(audit_store.id, audit_date)
 
-        audit = audit_store.audit
-
-        if audit_date < audit.audit_cycle.start_date or audit_date > audit.audit_cycle.end_date:
-            raise AppLogicError("audit date is out of range")
-        if audit_store.status != AuditStore.SUBMITTED:
-            raise AppLogicError("application cannot be approved right now")
-
-        audit_store.audit_date = audit_date
-        audit_store.save()
-        return audit_store
-    except AuditStore.DoesNotExist as e:
-        raise ObjectNotFound from e
 
 
 @atomic
 def unsubmit_for_moderator(audit_store_id, user_id):
-    try:
-        user = find_moderator_by_user_id(user_id)
-        audit_store = find_by_id_for_moderator(audit_store_id, user_id)
-
-
-        if audit_store.status == AuditStore.SUBMITTED:
-            audit_store.status = AuditStore.ASSIGNED
-            audit_store.save()
-            #TODO:VERB should be encapsulated
-            notify.send(
-                user,
-                recipient=Group.objects.get(name=GROUP_NAME_MANAGER),
-                verb='AUDIT_STORE_UNSUBMITTED',
-                action_object=audit_store,
-                target=audit_store.audit
-            )
-            notify.send(
-                user,
-                recipient=Group.objects.get(name=GROUP_NAME_MANAGER),
-                verb='AUDIT_STORE_UNSUBMITTED',
-                action_object=audit_store,
-                target=audit_store.audit
-            )
-            notify.send(
-                user,
-                recipient=audit_store.user,
-                verb='AUDIT_STORE_UNSUBMITTED',
-                action_object=audit_store,
-                target=audit_store.audit
-            )
-            return audit_store
-        else:
-            raise AppLogicError("audit store cannot be unsubmitted now")
-    except AuditStore.DoesNotExist as e:
-        raise ObjectNotFound from e
+    user = find_moderator_by_user_id(user_id)
+    audit_store = find_by_id_for_moderator(audit_store_id, user_id)
+    return audit_store_service.unsubmit(audit_store.id, user)
