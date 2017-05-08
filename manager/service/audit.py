da1b0590d55832e6ec8d3e1d97a7219af93b4b17
satyanash <satyajeet@webexports.co.in>
2017-05-08 14:26:07 +0530

    restrict web, telephone audits via location

--- manager/service/audit.py
+++ manager/service/audit.py
@@ -85,7 +85,7 @@ def get_available_audits_within_box(profileinfo_id, city_id=None, kms=None):
             lon_max, lon_min, lat_max, lat_min = geo.bounding_box(city.lat, city.lon, kms)
 
             available_audits = active_audits.filter(
-                Q(audit_cycle__type__in=[AuditCycle.WEB, AuditCycle.PHONE]) |
+                #Q(audit_cycle__type__in=[AuditCycle.WEB, AuditCycle.PHONE]) |
                 Q(
                     store__location__city__lat__lte=lat_max,
                     store__location__city__lat__gte=lat_min,
