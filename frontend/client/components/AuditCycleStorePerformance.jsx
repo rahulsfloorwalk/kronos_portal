00fed1c3e799d7e10d9aec7d97fccd5904c88f08
satyanash <satyajeet@webexports.co.in>
2017-05-24 14:41:48 +0530

    remove demo rendering data from time series and store performance graphs

--- frontend/client/components/AuditCycleStorePerformance.jsx
+++ frontend/client/components/AuditCycleStorePerformance.jsx
@@ -1,11 +1,24 @@
 import React from 'react';
 import { ResponsiveContainer, BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend} from 'recharts';
+
 import {demo} from '../../config.js';
+
 import {fetchAuditCycleStorePerformance} from '../service/audit_cycle.js';
+
+import Loading from '../../js/components/Loading.jsx';
 
 var AuditCycleStorePerformance = React.createClass({
 	getInitialState: function(){
-		return null;
+		return {
+			loading: false
+		};
+	},
+	setLoading: function(loading){
+		this.setState( prevState => {
+			return Object.assign({}, prevState, {
+				loading
+			});
+		});
 	},
 	create_structure: function(input_data){
     let data_arr = input_data.data;
@@ -45,6 +58,7 @@ var AuditCycleStorePerformance = React.createClass({
 
 	reloadData: function(auditType){
 		if(!demo){
+			this.setLoading(true);
 			let ts = fetchAuditCycleStorePerformance(this.props.auditType).then((reportData) => {
 				let ts_structure = this.create_structure(reportData);
 				this.setState({
@@ -54,7 +68,7 @@ var AuditCycleStorePerformance = React.createClass({
 					'type': this.props.type
 				});
 				this.create_bars();
-			});
+			}).always(() => this.setLoading(false));
 		}
 	},
 
@@ -66,61 +80,29 @@ var AuditCycleStorePerformance = React.createClass({
 	},
 
 	render : function(){
-    const data = [
-      {name: 'Welcome & Reception', January: 40, February: 60, March: 70},
-      {name: 'Need Analysis', January: 45, February: 40, March: 50, April: 22},
-      {name: 'Presentation', January: 60, February: 70, March: 75, April: 29},
-      {name: 'Test Drive', January: 70, February: 75, March: 75, April: 20},
-      {name: 'Negotiation & Follow up', January: 60, February: 50, March: 70, April: 21},
-      {name: 'Facility', January: 30, February: 60, March: 70, April: 25},
-      {name: 'Visit Confirmation', January: 50, February: 60, March: 65, April: 21},
-    ];
-    if (demo){
-			console.log('Rendering demo data')
-  		return(
-  			<div className="container">
-  				<div className="row">
-  					<br/>
-  					<br/>
-  					<div className="col-md-12">
-            <h1>Q4 - Audit cycle summary</h1>
-            <br/>
-  					<BarChart width={1000} height={300} data={data} margin={{top: 25, right: 30, left: 20, bottom: 5}}>
-  						<XAxis dataKey="name"/>
-  						<YAxis/>
-  						<Tooltip/>
-  						<Legend />
-  						<Bar dataKey="January" fill="#4ca9d7" label/>
-  						<Bar dataKey="February" fill="#0085c6" label/>
-              <Bar dataKey="March" fill="#005d8a" label/>
-  					</BarChart>
-  					</div>
-  				</div>
-  			</div>
-  		);
-    }
-    else if (this.state != null){
-	console.log('Rendering real store performance data');
-	return(
-	<div>
-		<h3 className="text-center">{this.state.title}</h3>
-		<ResponsiveContainer width="100%" aspect={3 / 1}>
-		<BarChart width={600} height={300} data={this.state.data} margin={{top: 25, right: 10, left: 10, bottom: 5}}>
+		let chart;
+		if(this.state.loading){
+			chart = <Loading/>;
+		} else {
+			chart = (
+			<ResponsiveContainer width="100%" aspect={3 / 1}>
+			<BarChart width={600} height={300} data={this.state.data} margin={{top: 25, right: 10, left: 10, bottom: 5}}>
 			<XAxis dataKey="name"/>
 			<YAxis label="Score" domain={[0,100]} tickFormatter={f => f + "%"}/>
 			<Tooltip formatter={v => v+"%"}/>
 			<Legend />
 			{this.state.bars}
-		</BarChart>
-		</ResponsiveContainer>
-	</div>
-	);
-	}
-		else{
-			console.debug('demo is false and no data available. hence chart not rendering')
-      return null;
+			</BarChart>
+			</ResponsiveContainer>
+			);
 		}
-	},
+		return(
+			<div>
+			<h3 className="text-center">{this.state.title}</h3>
+			{chart}
+			</div>
+		);
+	}
 });
 
 export default AuditCycleStorePerformance;
--- frontend/client/components/AuditCycleTimeSeries.jsx
+++ frontend/client/components/AuditCycleTimeSeries.jsx
@@ -1,11 +1,24 @@
 import React from 'react';
 import { ResponsiveContainer, BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend} from 'recharts';
+
 import {demo} from '../../config.js';
+
+import Loading from '../../js/components/Loading.jsx';
+
 import {fetchAuditCyclesTimeSeries} from '../service/audit_cycle.js';
 
 var AuditCycleTimeSeries = React.createClass({
 	getInitialState: function(){
-		return null;
+		return {
+			loading: false
+		};
+	},
+	setLoading: function(loading){
+		this.setState( prevState => {
+			return Object.assign({}, prevState, {
+				loading
+			});
+		});
 	},
 	create_structure: function(ts){
 		let data = [];
@@ -33,6 +46,7 @@ var AuditCycleTimeSeries = React.createClass({
 
 	reloadData: function(auditType){
 		if(!demo){
+			this.setLoading(true);
 			let ts = fetchAuditCyclesTimeSeries(auditType).then((reportData) => {
 				let ts_structure = this.create_structure(reportData);
 				this.setState({
@@ -41,7 +55,7 @@ var AuditCycleTimeSeries = React.createClass({
 					'title': reportData.title
 				});
 				this.create_bars();
-			});
+			}).always( () => this.setLoading(false));
 		}
 	},
 
@@ -53,61 +67,30 @@ var AuditCycleTimeSeries = React.createClass({
 	},
 
 	render : function(){
-    const data = [
-      {name: 'Welcome & Reception', January: 40, February: 60, March: 70},
-      {name: 'Need Analysis', January: 45, February: 40, March: 50, April: 22},
-      {name: 'Presentation', January: 60, February: 70, March: 75, April: 29},
-      {name: 'Test Drive', January: 70, February: 75, March: 75, April: 20},
-      {name: 'Negotiation & Follow up', January: 60, February: 50, March: 70, April: 21},
-      {name: 'Facility', January: 30, February: 60, March: 70, April: 25},
-      {name: 'Visit Confirmation', January: 50, February: 60, March: 65, April: 21},
-    ];
-    if (demo){
-			console.log('Rendering demo data')
-  		return(
-  			<div className="container">
-  				<div className="row">
-  					<br/>
-  					<br/>
-  					<div className="col-md-12">
-            <h1>Q4 - Audit cycle summary</h1>
-            <br/>
-  					<BarChart width={1000} height={300} data={data} margin={{top: 25, right: 30, left: 20, bottom: 5}}>
-  						<XAxis dataKey="name"/>
-  						<YAxis/>
-  						<Tooltip/>
-  						<Legend />
-  						<Bar dataKey="January" fill="#4ca9d7" label/>
-  						<Bar dataKey="February" fill="#0085c6" label/>
-              <Bar dataKey="March" fill="#005d8a" label/>
-  					</BarChart>
-  					</div>
-  				</div>
-  			</div>
-  		);
-    }
-    else if (this.state != null){
-      console.log('Rendering real time series data');
-			return(
-			<div>
-			<h3 className="text-center">{this.state.title}</h3>
+		let chart;
+		if(this.state.loading){
+			chart = <Loading/>;
+		} else {
+			chart = (
 			<ResponsiveContainer width="100%" aspect={3 / 1}>
 			<BarChart width={1000} height={300} data={this.state.data} margin={{top: 25, right: 30, left: 50, bottom: 5}}>
-				<YAxis label="Score" type="number" domain={[0,100]} tickFormatter={f => f + "%"}/>
-				<XAxis dataKey="name" type="category"/>
-				<Tooltip formatter={v => v + "%"}/>
-				<Legend />
-				{this.state.bars}
+			<YAxis label="Score" type="number" domain={[0,100]} tickFormatter={f => f + "%"}/>
+			<XAxis dataKey="name" type="category"/>
+			<Tooltip formatter={v => v + "%"}/>
+			<Legend verticalAlign="top"/>
+			{this.state.bars}
 
 			</BarChart>
 			</ResponsiveContainer>
-			</div>
-  		);
-    }
-		else{
-			console.debug('demo is false and no data available. hence chart not rendering')
-      return null;
+			);
 		}
+		console.debug('Rendering real time series data');
+		return(
+			<div>
+				<h3 className="text-center">{this.state.title}</h3>
+				{chart}
+			</div>
+		);
 	},
 });
 
