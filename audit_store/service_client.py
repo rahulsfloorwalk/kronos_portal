79b4117bc7c25df2744a9d307491158bba9a873d
satyanash <satyajeet@webexports.co.in>
2017-06-11 09:56:00 +0530

    hide previous audit stores in upcoming audits view in client UI

--- audit_store/service_client.py
+++ audit_store/service_client.py
@@ -1,8 +1,13 @@
 from .models import AuditStore
+from datetime import date
 
 
 def find_upcoming_for_client(client_id):
-    return AuditStore.objects.filter(audit__audit_cycle__client_id=client_id, status=AuditStore.ASSIGNED).order_by('audit_date')
+    return AuditStore.objects.filter(
+            audit__audit_cycle__client_id=client_id,
+            status=AuditStore.ASSIGNED,
+            audit_date__gte=date.today(),
+        ).order_by('audit_date')
 
 
 def find_by_id_for_client(audit_store_id, client_id):
