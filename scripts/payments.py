dbe21a2d9acc2141bb9123f43378344a8ea75d8d
argon <xamit.94@gmail.com>
2017-06-18 05:12:07 +0530

    add script to convert completed reports to accepted reports and add payment row for each

--- scripts/payments.py
+++ scripts/payments.py
@@ -1,4 +1,5 @@
 from payment.models import Payment
+from audit.models import AuditCycle
 from payment.service.payment_manager import add_payment_on_audit_store_accepted as insert_payment
 from audit_store.models import AuditStore
 
@@ -7,3 +8,12 @@ def insert_payments_for_complete_reports():
     for audit_store in audit_stores:
         if audit_store.status == AuditStore.COMPLETED:
             insert_payment(audit_store.id)
+
+def accept_reports_add_payments():
+    audit_cycle_status = [AuditCycle.REPORT, AuditCycle.ARCHIVED]
+    audit_stores = AuditStore.objects.presentable().filter(audit__audit_cycle__status__in=audit_cycle_status)
+    print (len(audit_stores))
+    for a in audit_stores:
+        a.status = AuditStore.ACCEPTED
+        a.save()
+        insert_payment(a.id)
