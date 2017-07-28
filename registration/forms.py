0b41ba89ebe651a1ae3badf3942aa718f1361c7b
satyanash <satyajeet@webexports.co.in>
2017-07-28 13:12:30 +0530

    fix key name in signup form code

--- registration/forms.py
+++ registration/forms.py
@@ -44,7 +44,7 @@ class SignUpForm(UserCreationForm):
             self.add_error("phone", "a user with phone {} already exists".format(self.data["phone"]))
             valid = False
 
-        if self.data["referral_code"]:
+        if self.data.get("referred_by"):
             if not AdditionalInfo.objects.filter(referral_code=self.data["referred_by"].lower()).exists():
                 self.add_error("referred_by", "a user with referral code {} does not exists. Please enter valid referral code or leave blank.".format(self.data["referred_by"]))
                 valid = False
