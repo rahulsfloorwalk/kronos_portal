c20a1b6a3f2823b1d8bbe001778c9fe59a5bafce
satyanash <satyajeet@webexports.co.in>
2017-08-03 23:16:31 +0530

    add fake impact factor for axis bank

--- frontend/client/components/FakeImpactFactor.jsx
+++ frontend/client/components/FakeImpactFactor.jsx
@@ -20,6 +20,12 @@ var FakeImpactFactor = React.createClass({
 				{name: 'Operational Smoothness', "April 2017": 68.62, "May 2017": 75},
 				{name: 'Staff Loyalty', "April 2017": 52.33, "May 2017": 90.49},
 			],
+			data_18: [
+				{name: 'WOW Factor', "August 2017": 71},
+				{name: 'Customer Retention', "August 2017": 66},
+				{name: 'Brand Image', "August 2017": 75},
+				{name: 'Operational Smoothness', "August 2017": 70},
+			],
 		};
 	},
 	setLoading: function(loading){
@@ -62,6 +68,21 @@ var FakeImpactFactor = React.createClass({
 			<Legend wrapperStyle={{ top: 0}} verticalAlign="top"/>
 			<Bar barSize={40} dataKey="April 2017" fill={colors1[1]} label={v => <Text {...v} children={v.value === null ? "N/A" : v.value+"%"}/>}/>
 			<Bar barSize={40} dataKey="May 2017" fill={colors1[2]} label={v => <Text {...v} children={v.value === null ? "N/A" : v.value+"%"}/>}/>
+			</BarChart>
+			</ResponsiveContainer>
+			);
+		} else if(this.state.clientUser && this.state.clientUser.client.id === 18 && this.props.auditType === "WALKIN") {
+			let colors = ["#fcf2cf", "#f6d96f", "#f0bf0f", "#907309"];
+			let colors1 = ["#ffad33", "#ff9900", "#cc7a00"];
+			let colors2 = ["#dc4c46", "#eb9794"];
+			chart = (
+			<ResponsiveContainer width="100%" aspect={3 / 1}>
+			<BarChart data={this.state.data_18} margin={{top: 25, right: 5, left: 5, bottom: 30}}>
+			<YAxis label="Score" type="number" domain={[0,100]} tickFormatter={f => f + "%"}/>
+			<XAxis dataKey="name" type="category" tick={this.tickFunction} interval={0}/>
+			<Tooltip formatter={v => v === null ? "N/A" : v+"%"}/>
+			<Legend wrapperStyle={{ top: 0}} verticalAlign="top"/>
+			<Bar barSize={40} dataKey="August 2017" fill={colors1[1]} label={v => <Text {...v} children={v.value === null ? "N/A" : v.value+"%"}/>}/>
 			</BarChart>
 			</ResponsiveContainer>
 			);
