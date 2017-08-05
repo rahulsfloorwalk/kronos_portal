c8bd619dcb1c72ec987ea08cc2dd88e2b49b9fe9
satyanash <satyajeet@webexports.co.in>
2017-08-05 12:20:29 +0530

    fix max length for Attachment.mime_type

--- attachment/models.py
+++ attachment/models.py
@@ -29,7 +29,7 @@ class Attachment(Model):
     id = AutoField(db_column='id', primary_key=True)
     file_slug = CharField(db_column='file_slug', max_length=200, blank=False)
     proof_type = CharField(db_column='proof_type', max_length=20, choices=PROOF_TYPE, blank=False)
-    mime_type = CharField(db_column='mime_type', max_length=50, blank=False)
+    mime_type = CharField(db_column='mime_type', max_length=256, blank=False)
     file_name = CharField(db_column='file_name', max_length=256, blank=False)
     file_size = IntegerField(db_column='file_size')
     status = CharField(db_column='status', max_length=20, choices=STATUS, blank=False)
