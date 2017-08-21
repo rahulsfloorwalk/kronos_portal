57129576f9ad3912020556d8071196f73eafc70a
satyanash <satyajeet@webexports.co.in>
2017-08-22 00:03:06 +0530

    add impact factor for SMaaash

--- frontend/client/components/FakeImpactFactor.jsx
+++ frontend/client/components/FakeImpactFactor.jsx
@@ -26,6 +26,38 @@ var FakeImpactFactor = React.createClass({
 				{name: 'Brand Image', "August 2017": 75},
 				{name: 'Operational Smoothness', "August 2017": 70},
 			],
+			data_9: [
+				{
+					name: 'Business Visibility',
+					"July 2017": 88,
+					"August 2017": 94,
+				},
+				{
+					name: 'WOW Factor',
+					"July 2017": 45,
+					"August 2017": 62,
+				},
+				{
+					name: 'Brand Image',
+					"July 2017": 82,
+					"August 2017": 90,
+				},
+				{
+					name: 'Customer Retention',
+					"July 2017": 60,
+					"August 2017": 73,
+				},
+				{
+					name: 'Operational Smoothness',
+					"July 2017": 71,
+					"August 2017": 78,
+				},
+				{
+					name: 'Code of Conduct',
+					"July 2017": 82,
+					"August 2017": 87,
+				},
+			],
 		};
 	},
 	setLoading: function(loading){
@@ -83,6 +115,22 @@ var FakeImpactFactor = React.createClass({
 			<Tooltip formatter={v => v === null ? "N/A" : v+"%"}/>
 			<Legend wrapperStyle={{ top: 0}} verticalAlign="top"/>
 			<Bar barSize={40} dataKey="August 2017" fill={colors1[1]} label={v => <Text {...v} children={v.value === null ? "N/A" : v.value+"%"}/>}/>
+			</BarChart>
+			</ResponsiveContainer>
+			);
+		} else if(this.state.clientUser && this.state.clientUser.client.id === 9 && this.props.auditType === "WALKIN") {
+			let colors = ["#fcf2cf", "#f6d96f", "#f0bf0f", "#907309"];
+			let colors1 = ["#ffad33", "#ff9900", "#cc7a00"];
+			let colors2 = ["#dc4c46", "#eb9794"];
+			chart = (
+			<ResponsiveContainer width="100%" aspect={3 / 1}>
+			<BarChart data={this.state.data_9} margin={{top: 25, right: 5, left: 5, bottom: 30}}>
+			<YAxis label="Score" type="number" domain={[0,100]} tickFormatter={f => f + "%"}/>
+			<XAxis dataKey="name" type="category" tick={this.tickFunction} interval={0}/>
+			<Tooltip formatter={v => v === null ? "N/A" : v+"%"}/>
+			<Legend wrapperStyle={{ top: 0}} verticalAlign="top"/>
+			<Bar barSize={40} dataKey="July 2017" fill={colors1[1]} label={v => <Text {...v} children={v.value === null ? "N/A" : v.value+"%"}/>}/>
+			<Bar barSize={40} dataKey="August 2017" fill={colors1[2]} label={v => <Text {...v} children={v.value === null ? "N/A" : v.value+"%"}/>}/>
 			</BarChart>
 			</ResponsiveContainer>
 			);
