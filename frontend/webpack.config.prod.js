66c4d0f730c75c416cda8c6388e13f7dc05aa5ac
satyanash <satyajeet@webexports.co.in>
2017-08-22 16:17:49 +0530

    add chunkhash for cache busting to webpack prod config

--- frontend/webpack.config.prod.js
+++ frontend/webpack.config.prod.js
@@ -3,9 +3,15 @@ var webpack = require('webpack');
 var baseConfig = require('./webpack.config');
 
 module.exports = Object.assign({}, baseConfig, {
-	plugins : baseConfig.plugins.concat( new webpack.DefinePlugin({
-		"process.env": {
-			NODE_ENV: JSON.stringify("production")
-		}
-	}))
+	output: Object.assign({}, baseConfig.output, {
+		filename: '[name].[chunkhash].bundle.js',
+	}),
+	plugins : baseConfig.plugins.concat([
+		new webpack.DefinePlugin({
+			"process.env": {
+				NODE_ENV: JSON.stringify("production")
+			}
+		}),
+		new webpack.HashedModuleIdsPlugin(),
+	])
 });
