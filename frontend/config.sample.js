baf4415d368a966ff431326f67d01dcbe6adfb37
satyanash <satyajeet@webexports.co.in>
2017-09-22 13:12:40 +0530

    use const and inline exports in config.js

--- frontend/config.sample.js
+++ frontend/config.sample.js
@@ -2,25 +2,24 @@
  * define configuration options here
  */
 
-var url = {
+export const url = {
 	protocol : 'http',
 	hostname : 'localhost',
 	port : '8000',
 	api_base_path: "/"
 };
 
-var momentDateFormat = "Do MMM YYYY";
-var momentDateTimeFormat = "HH:mm Do MMM YYYY";
+export const momentDateFormat = "Do MMM YYYY";
+export const momentDateTimeFormat = "HH:mm Do MMM YYYY";
 
-const auditorGAId = "UA-XXXXXXX-X";
+export const auditorGAId = "UA-XXXXXXX-X";
 
-const impactFactorDataUrl = "http://localhost:8080/random_file.js";
+export const impactFactorDataUrl = "http://localhost:8080/random_file.js";
 
-const demo = true;
+export const demo = true;
 
-const facebook_client_id = "122450261725178"
+export const facebook_client_id = "122450261725178"
 
-const facebook_fields = "id,name,first_name,last_name,gender,age_range,email,picture,likes,friends,location,birthday,link,verified";
-const facebook_scope = "email,public_profile,user_friends,user_likes,user_location,user_birthday";
+export const facebook_fields = "id,name,first_name,last_name,gender,age_range,email,picture,likes,friends,location,birthday,link,verified";
+export const facebook_scope = "email,public_profile,user_friends,user_likes,user_location,user_birthday";
 
-export { url, momentDateFormat, auditorGAId, momentDateTimeFormat, demo, facebook_client_id, facebook_scope, facebook_fields };
