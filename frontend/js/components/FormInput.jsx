7028b848024392ec4696fbada00e2d7efba6383c
satyanash <satyajeet@webexports.co.in>
2017-09-23 01:08:21 +0530

    add Save and Next feature to Section Form

--- frontend/js/components/FormInput.jsx
+++ frontend/js/components/FormInput.jsx
@@ -6,11 +6,14 @@ import FormErrorList from './FormErrorList.jsx';
 
 
 var FormInput = React.createClass({
+	focus: function(){
+		this._input && this._input.focus();
+	},
 	render : function(){
 		return (
 			<FormGroup>
 				<label>{this.props.label}</label>
-				<input className="form-control" {...this.props}/>
+				<input className="form-control" ref={r => this._input = r} {...this.props}/>
 				<FormErrorList errors={this.props.errors}/>
 			</FormGroup>
 		);
--- frontend/js/components/manager/SectionForm.jsx
+++ frontend/js/components/manager/SectionForm.jsx
@@ -18,7 +18,7 @@ var SectionForm = React.createClass({
 		return {};
 	},
 	componentDidMount: function() {
-		console.log("componentDidMount(...) called with props.params as", this.props.params);
+		//console.log("componentDidMount(...) called with props.params as", this.props.params);
 		if(this.props.params.sectionId){
 			this.props.dispatch(loadSectionEditForm(this.props.params.sectionId));
 		} else {
@@ -29,7 +29,7 @@ var SectionForm = React.createClass({
 		});
 	},
 	componentWillReceiveProps: function(nextProps) {
-		console.log("componentWillReceiveProps(...) called with",nextProps);
+		//console.log("componentWillReceiveProps(...) called with",nextProps);
 		if( nextProps.section){
 			this.setState(nextProps.section);
 		}
@@ -43,7 +43,6 @@ var SectionForm = React.createClass({
 	onSubmit: function(e){
 		e.preventDefault();
 		var submitPromise;
-		console.log(this.state);
 		if(this.props.params.sectionId){
 			submitPromise = this.props.dispatch(saveSectionEditForm(this.state));
 		} else {
@@ -54,14 +53,35 @@ var SectionForm = React.createClass({
 			Alert.success("SECTION SAVED");
 		});
 	},
+	saveAndNext: function(e){
+		e.preventDefault();
+		let submitPromise;
+		if(this.props.params.sectionId){
+			submitPromise = this.props.dispatch(saveSectionEditForm(this.state));
+		} else {
+			submitPromise = this.props.dispatch(saveSectionAddForm(this.state));
+		}
+		submitPromise.then((savedSection) => {
+			this.setState({
+				sequence: savedSection.sequence + 1,
+				name: "",
+			});
+			hashHistory.push(this.props.location.pathname);
+			Alert.success("SECTION SAVED");
+			this._nameInput && this._nameInput.focus();
+		});
+	},
 	render : function(){
 		var modalTitle = this.props.params.auditCycleId ? "Edit Section" : "Add Section";
 		return (
 			<Modal modalTitle={modalTitle} onClose={hashHistory.goBack}>
 				<form onSubmit={this.onSubmit}>
 					<FormInput label="Sequence" min="1" type="number" value={this.state.sequence} name="sequence" onChange={this.inputChanged} errors={this.props.errors.sequence}/>
-					<FormInput label="Section Name" maxLength="50" type="text" value={this.state.name} name="name" onChange={this.inputChanged} errors={this.props.errors.name}/>
-					<SaveButton/>
+					<FormInput label="Section Name" maxLength="50" type="text" value={this.state.name} name="name" onChange={this.inputChanged} errors={this.props.errors.name} ref={r => this._nameInput = r}/>
+					<SaveButton/>&nbsp;
+					{ ! this.props.params.sectionId ?
+					<button type="button" className="btn btn-primary" onClick={this.saveAndNext}>Save and Next</button>
+					: null }
 				</form>
 			</Modal>
 		);
@@ -69,7 +89,7 @@ var SectionForm = React.createClass({
 });
 
 var mapStoreToProps = function(store, ownProps){
-	console.debug("ownProps",ownProps);
+	//console.debug("ownProps",ownProps);
 	return {
 		section: store.sections[ownProps.params.sectionId],
 		errors: store.forms.section.errors,
