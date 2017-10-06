6c5fbcac3e556786b177c3882d1583ab626c96f1
satyanash <satyajeet@webexports.co.in>
2017-10-06 09:59:12 +0530

    improve audit apply form to show only calendar

--- frontend/js/components/auditor/AuditApplyForm.jsx
+++ frontend/js/components/auditor/AuditApplyForm.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import $ from 'jquery';
 import * as ReactRedux from 'react-redux';
 import { hashHistory } from 'react-router';
+import Datetime from 'react-datetime';
 
 import moment from 'moment';
 
@@ -23,7 +24,12 @@ import Loading from '../Loading.jsx';
 
 var AuditApplyForm = React.createClass({
 	getInitialState: function(){
-		return {};
+		return {
+			submitting: false,
+		};
+	},
+	setSubmitting: function(submitting){
+		this.setState((prevState) => Object.assign({}, prevState, { submitting }));
 	},
 	componentDidMount: function() {
 		this.props.dispatch(loadAuditApplyForm(this.props.params.auditId));
@@ -37,25 +43,40 @@ var AuditApplyForm = React.createClass({
 	},
 	onSubmit: function(e){
 		e.preventDefault();
-		var obj = {
+		this.setSubmitting(true);
+		let promise = this.props.dispatch(submitAuditApplyForm({
 			audit_id: this.props.audit.id,
-			audit_location_id: this.props.auditLocationId,
-			audit_date: this.state.audit_date ? this.state.audit_date.format("YYYY-MM-DD") : ""
-		};
-		var promise = this.props.dispatch(submitAuditApplyForm(obj));
-		promise.then(() => hashHistory.push(`/audit/cycle/${this.props.audit.audit_cycle.id}`));
+			audit_date: this.state.audit_date ? this.state.audit_date.format("YYYY-MM-DD") : "",
+		})).done(() => hashHistory.push(`/audit/cycle/${this.props.audit.audit_cycle.id}`)).always(() => this.setSubmitting(false));
+	},
+	isValidDate: function(currentDate, selectedDate){
+		let startDate = moment(this.props.audit.audit_cycle.start_date);
+		let endDate = moment(this.props.audit.audit_cycle.end_date);
+		return currentDate.isBetween(startDate, endDate, null, '[]'); //inclusive
 	},
 	render : function(){
 		return (
-			<Modal modalTitle="Apply for Audit" onClose={hashHistory.goBack}>
+			<Modal modalTitle={`Apply for Audit at ${this.props.audit.store.location.city.name}`} onClose={hashHistory.goBack}>
 				<form onSubmit={this.onSubmit}>
 					<FormErrorList errors={this.props.errors.non_field_errors}/>
-					<p><label>Audit Type:</label> { getAuditType(this.props.audit.audit_cycle.type) }</p>
-					<p><label>Start Date:</label> { moment(this.props.audit.audit_cycle.start_date).format(momentDateFormat) }</p>
-					<p><label>End Date:</label> { moment(this.props.audit.audit_cycle.end_date).format(momentDateFormat) }</p>
-					<p><label>Location:</label> { this.props.audit.store.location.name }, { this.props.audit.store.location.city.name }</p>
-					<FormDateInput label="Preferred Audit Date" value={this.state.audit_date} name="audit_date" onChange={this.dateChanged} errors={this.props.errors.audit_date}/>
-					<SaveButton text="Apply"/>
+					<p className="text-center">
+						<big>{ moment(this.props.audit.audit_cycle.start_date).format(momentDateFormat) }</big> to <big>{ moment(this.props.audit.audit_cycle.end_date).format(momentDateFormat) }</big>
+					</p>
+					<style dangerouslySetInnerHTML={{__html:".rdtPicker{ margin-left:auto; margin-right:auto; }"}}/>
+					<Datetime style={{marginLeft:"auto",marginRight:"auto"}}
+						timeFormat={false} dateFormat="YYYY-MM-DD"
+						closeOnSelect={false} closeOnTab={false} disableOnClickOutside={true}
+						open={true} input={false}
+						name="audit_date"
+						value={this.state.audit_date}
+						onChange={this.dateChanged}
+						isValidDate={this.isValidDate}
+					/>
+					<FormErrorList errors={this.props.errors.audit_date}/>
+					<button className="btn btn-lg btn-primary btn-block" disabled={!this.state.audit_date || this.state.submitting}>
+						{ this.state.submitting ? "applying..." :
+						<span>Apply{ this.state.audit_date ? <span> for <b>{this.state.audit_date.format(momentDateFormat)}</b></span> : null }</span> }
+					</button>
 				</form>
 			</Modal>
 		);
