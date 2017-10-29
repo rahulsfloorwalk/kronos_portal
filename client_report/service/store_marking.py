6b42a2e5ef81993aa65dfdf1dda4208df534afbb
satyanash <satyajeet@webexports.co.in>
2017-10-30 02:15:03 +0530

    prefetch and optimize store marking trends report

--- client_report/service/store_marking.py
+++ client_report/service/store_marking.py
@@ -1,17 +1,24 @@
-from django.db.models import Q
+from django.db.models import Prefetch
 from audit_store.models import AuditStore
-from audit.models import AuditCycle, Audit
+from audit.models import AuditCycle
 from questionnaire.models import Question
 from answer.models import Answer
 
 def get_scores_for_store(store_id, client_id, audit_type):
     all_cycles = AuditCycle.objects.filter(client_id=client_id).filter(type=audit_type).order_by('end_date')
-    if all_cycles.count() > 5:
-        audit_cycles = all_cycles[all_cycles.count-5:]
+    all_cycle_count = all_cycles.count()
+    if all_cycle_count > 5:
+        audit_cycles = all_cycles[all_cycle_count - 5:]
     else:
         audit_cycles = all_cycles
+
     audit_cycle_master = [audit_cycle.name for audit_cycle in audit_cycles]
-    master_questions = Question.objects.filter(section__audit_cycle__id=audit_cycles[len(audit_cycles)-1].id).order_by('section__sequence').order_by('sequence')
+
+    master_questions = Question.objects.filter(section__audit_cycle__id=audit_cycles[len(audit_cycles)-1].id) \
+        .order_by('section__sequence') \
+        .order_by('sequence') \
+        .select_related('section')
+
     raw_data = []
     for audit_cycle in audit_cycles:
         obj = {}
@@ -25,7 +32,15 @@ def get_scores_for_store(store_id, client_id, audit_type):
     return response_data
 
 def get_question_wise_marks_for_audit_cycle(audit_cycle_id, store_id):
-    questions = Question.objects.filter(section__audit_cycle__id=audit_cycle_id)
+    questions = Question.objects.filter(section__audit_cycle__id=audit_cycle_id) \
+        .select_related('section') \
+        .prefetch_related(
+            Prefetch('answers', queryset=Answer.objects.filter(
+                audit_store__status__in=(AuditStore.COMPLETED, AuditStore.ACCEPTED),
+                audit_store__audit__store_id=store_id,
+            )),
+    )
+
     response_data = []
     for question in questions:
         question_object = {}
@@ -33,28 +48,23 @@ def get_question_wise_marks_for_audit_cycle(audit_cycle_id, store_id):
         question_object['section_name'] = question.section.name
         question_object['section_sequence'] = question.section.sequence
         question_object['sequence'] = question.sequence
-        question_object['score'] = get_average_score_for_question_in_audit_cycle(question.id, store_id)
+        question_object['score'] = get_average_score_for_question_in_audit_cycle(question, store_id)
         response_data.append(question_object)
     return response_data
 
-def get_average_score_for_question_in_audit_cycle(question_id, store_id):
+def get_average_score_for_question_in_audit_cycle(question, store_id):
 
-    answers = Answer.objects.filter(question_id=question_id)\
-        .filter(Q(audit_store__status=AuditStore.COMPLETED) | Q(audit_store__status=AuditStore.ACCEPTED))\
-        .filter(audit_store__audit__store__id=store_id)
-    if len(answers) > 0:
-        total = 0
-        count = 0
-        for answer in answers:
-            if answer.marks_obtained is not None:
-                total += answer.marks_obtained
-                count += 1
-        if count > 0:
-            average = total/count
-        else:
-            average = -1
-    else:
-        average = -1
+    total = 0
+    count = 0
+    average = -1
+
+    for answer in question.answers.all():
+        if answer.marks_obtained is not None:
+            total += answer.marks_obtained
+            count += 1
+    if count > 0:
+        average = total / count
+
     return average
 
 def transpose_data(master_questions, audit_cycle_scores):
