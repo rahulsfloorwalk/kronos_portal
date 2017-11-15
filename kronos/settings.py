3aa8cce746dedbe90b09b1782f264f800c559d18
satyanash <satyajeet@webexports.co.in>
2017-11-15 18:17:55 +0530

    fix ALLOWED_HOSTS to work behind a proxy

--- kronos/settings.py
+++ kronos/settings.py
@@ -45,7 +45,7 @@ RHEA_DOMAIN = properties["RHEA"]["RHEA_DOMAIN"]
 RHEA_BASE_URL = "{}://{}".format(RHEA_PROTOCOL, RHEA_DOMAIN)
 
 # only allow requests with Host: KRONOS_DOMAIN.split(':')[0]
-ALLOWED_HOSTS = [KRONOS_DOMAIN.split(':')[0],]
+ALLOWED_HOSTS = ["localhost", KRONOS_DOMAIN.split(':')[0]]
 
 BRAND_NAME = properties["GENERAL"]["BRAND_NAME"]
 BRAND_SHORTNAME = properties["GENERAL"]["BRAND_SHORTNAME"]
