b74b1413226e5814dfc5f343d28bfe5ccb839191
satyanash <satyajeet@webexports.co.in>
2017-11-15 19:16:33 +0530

    update setup.py to reflect new configuration

--- setup.py
+++ setup.py
@@ -1,22 +1,13 @@
 import os
 import sys
-import logging
 
 from subprocess import call
 from setuptools import setup, find_packages
 
 version = '1.0.0'
 
-_logger = logging.getLogger(__name__)
-
 if sys.argv[-1] == 'build_frontend':
-    _logger.info("cd frontend/")
     os.chdir('frontend')
-
-    if not os.path.isfile('config.js'):
-        logger.error("cannot build frontend without config.js")
-        sys.exit(1)
-
     call(["npm","install"])
     call(["npm","run","build-prod"])
     os.chdir('..')
