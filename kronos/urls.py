157ccaa5fe721c61e88a9b38d4309d8c1b180917
satyanash <satyajeet@webexports.co.in>
2017-12-04 19:06:54 +0530

    fix debug toolbar condition and new urls in opportunity emails

--- kronos/urls.py
+++ kronos/urls.py
@@ -34,7 +34,7 @@ urlpatterns = [
     url(r'^moderator/', include(moderator_urls.urlpatterns)),
 ]
 
-if settings.DEBUG:
+if settings.DEBUG and settings.DEBUG_TOOLBAR:
     import debug_toolbar
     urlpatterns = [
         url(r'^__debug__/', include(debug_toolbar.urls)),
