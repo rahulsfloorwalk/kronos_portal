8de511596ac4e31560209c0929470c6bf1cf269b
satyanash <satyajeet@webexports.co.in>
2017-12-04 18:19:09 +0530

    add django admin action to fetch tweets for a handle

--- social/admin.py
+++ social/admin.py
@@ -1,8 +1,27 @@
 from django.contrib import admin
+from django.contrib.admin import ModelAdmin
 
 from .models import Facebook, TwitterFeed, TwitterHandle
+
+from .service import twitter_client
 
 # Register your models here.
 admin.site.register(Facebook)
 admin.site.register(TwitterFeed)
-admin.site.register(TwitterHandle)
+
+
+def fetch_tweets(modeladmin, request, handles):
+    count = 0
+    for handle in handles:
+        count += len(twitter_client.save_tweets_for_handle(handle))
+    msg = "fetched {} tweets for {} handles".format(count, len(handles))
+    modeladmin.message_user(request, msg)
+
+
+fetch_tweets.short_description = "Fetch Tweets for selected handles"
+
+class TwitterHandleAdmin(ModelAdmin):
+    actions = [fetch_tweets]
+
+
+admin.site.register(TwitterHandle, TwitterHandleAdmin)
