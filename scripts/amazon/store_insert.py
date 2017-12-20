50877769927f32c8f3a677188bc433a430a36343
satyanash <satyajeet@webexports.co.in>
2017-12-20 16:33:13 +0530

    change amazon store insert script to insert new stores

--- scripts/amazon/store_insert.py
+++ scripts/amazon/store_insert.py
@@ -1,34 +1,61 @@
 import csv
 from django.db import transaction
 from client.models import Store, Client
-from manager.models import Location
+from manager.models import Location, City
 from audit.models import AuditCycle, Audit
 
 @transaction.atomic
+def insert_stores_and_audits(audit_cycle_id, filename):
+    audit_cycle = AuditCycle.objects.get(pk=audit_cycle_id)
+
+    inserted_stores = insert_stores(audit_cycle.client_id, filename)
+    inserted_audits = insert_audits_for_stores(audit_cycle_id, inserted_stores)
+
+    return inserted_stores, inserted_audits
+
+
+@transaction.atomic
 def insert_stores(client_id, filename):
-    stores = Store.objects.filter(client_id=client_id)
-    if(len(stores) > 0):
-        print("Stores already present for client with id: ", client_id)
-        return
+
+    extra_columns = ('REGION', 'STATE', 'PINCODE', 'STATUS', 'CRM', 'CDM', 'ADDRESS1', 'ADDRESS2', 'STATION', 'TYPE')
 
     amazon = Client.objects.get(pk=client_id)
+
+    inserted_stores = []
+
     with open(filename) as csvfile:
         reader = csv.DictReader(csvfile)
         for row in reader:
             store = Store()
-            locations = Location.objects.filter(name__iexact=row['city'], city__name__iexact=row['city'])
-            store.name = row['name']
+            city = City.objects.get(name__iexact=row['CITY'])
+            locations = Location.objects.filter(name__iexact=row['CITY'], city=city)
+            store.name = row['NAME']
             store.client = amazon
-            store.code = row['code']
-            store.type = row['type']
-            store.address = row['address']
-            store.phone = row['phone']
+            store.code = row['CODE']
+            store.type = row['TYPE']
+            store.address = "{} ; {}".format(row['ADDRESS1'], row['ADDRESS2'])
+            store.phone = row['PHONE']
             store.location = locations[0]
+            store.city = city
+            store.extra_data = {col:row[col] for col in extra_columns}
             store.save()
+            inserted_stores.append(store)
 
+    return inserted_stores
+
+@transaction.atomic
 def insert_all_audits(audit_cycle_id):
     audit_cycle = AuditCycle.objects.get(pk=audit_cycle_id)
     stores = Store.objects.filter(client=audit_cycle.client)
+
+    return insert_audits_for_stores(audit_cycle_id, stores)
+
+
+@transaction.atomic
+def insert_audits_for_stores(audit_cycle_id, stores):
+    audit_cycle = AuditCycle.objects.get(pk=audit_cycle_id)
+
+    inserted_audits = []
 
     for store in stores:
         audit = Audit()
@@ -38,3 +65,6 @@ def insert_all_audits(audit_cycle_id):
         audit.store = store
         audit.audit_cycle = audit_cycle
         audit.save()
+        inserted_audits.append(audit)
+
+    return inserted_audits
