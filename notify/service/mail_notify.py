1a1ed0ab13ede18e147dcbdf13579c437361adcf
satyanash <satyajeet@webexports.co.in>
2018-01-16 21:10:05 +0530

    fix email templates for waitlisting

--- notify/service/mail_notify.py
+++ notify/service/mail_notify.py
@@ -84,7 +84,7 @@ def notification_email_task(notif_id):
             params['txt_template'] = 'notify/reject_email.txt'
 
         elif notif.verb == verbs.AUDIT_APPLICATION_WAITLISTED:
-            subject = "Audit Application has been wait listed {}".format(params['client'])
+            subject = "Audit Application has been wait listed for {}".format(params['client'])
             params['html_template'] = 'notify/waitlist_email.html'
             params['txt_template'] = 'notify/waitlist_email.txt'
 
