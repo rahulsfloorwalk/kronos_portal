705f9a3efc231ac8acec439274735e98c00619e1
satyanash <satyajeet@webexports.co.in>
2018-02-12 12:43:02 +0530

    group reports by client on moderator dashboard

--- frontend/js/moderator/components/AuditStoreDashboard.jsx
+++ frontend/js/moderator/components/AuditStoreDashboard.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import PropTypes from "prop-types";
 import { hashHistory } from "react-router";
 
 import moment from "moment";
@@ -14,6 +15,11 @@ import Jumbotron from "../../components/Jumbotron.jsx";
 import { findPending, findCompleted } from "../service/audit_store.js";
 
 class AuditStoreList2 extends Component{
+	static propTypes = {
+		auditStores: PropTypes.array,
+		client: PropTypes.object,
+	};
+
 	constructor(props){
 		super(props);
 		this.state = {
@@ -41,7 +47,7 @@ class AuditStoreList2 extends Component{
 		return(
 			<div className="panel panel-default">
 				<div className="panel-heading">
-					<h4 className="">{this.props.auditStores.length} Reports</h4>
+					<h4 className="">{this.props.auditStores[0] && this.props.auditStores[0].audit.audit_cycle.client.name} - {this.props.auditStores.length} Reports</h4>
 				</div>
 				<table className="table table-hover table-striped">
 					<thead>
@@ -65,7 +71,36 @@ class AuditStoreList2 extends Component{
 	}
 }
 
+class AuditStoreTables extends Component {
+	static propTypes = {
+		auditStores: PropTypes.array,
+	};
+
+	render(){
+		let client_dict = {};
+		for(let as of this.props.auditStores){
+			if(!client_dict[as.audit.audit_cycle.client.id]){
+				client_dict[as.audit.audit_cycle.client.id] = [];
+			}
+			client_dict[as.audit.audit_cycle.client.id].push(as);
+		}
+
+		let tables = [];
+		for(let client_id in client_dict){
+			tables.push(<AuditStoreList2 key={client_id} auditStores={client_dict[client_id]}/>);
+		}
+		return (<div>
+			<h2 className="page-header">{this.props.auditStores.length} Reports</h2>
+			{tables}
+		</div>);
+	}
+}
+
 export default class AuditStoreDashboard extends Component {
+	static propTypes = {
+		location: PropTypes.object,
+	};
+
 	constructor(props){
 		super(props);
 		this.state = {
@@ -83,18 +118,20 @@ export default class AuditStoreDashboard extends Component {
 	}
 
 	componentWillReceiveProps(nextProps){
-		if(nextProps.location.query.type === "qa_done"){
-			findCompleted().then(auditStores => this.setState({qa_done: auditStores}));
-		} else {
-			findPending().then(auditStores => this.setState({qa_pending: auditStores}));
+		if(this.props.location.query.type !== nextProps.location.query.type){
+			if( nextProps.location.query.type === "qa_done"){
+				findCompleted().then(auditStores => this.setState({qa_done: auditStores}));
+			} else {
+				findPending().then(auditStores => this.setState({qa_pending: auditStores}));
+			}
 		}
 	}
 
 	render(){
 		if(this.props.location.query.type === "qa_done"){
-			return <AuditStoreList2 auditStores={this.state.qa_done}/>;
+			return <AuditStoreTables auditStores={this.state.qa_done}/>;
 		} else {
-			return <AuditStoreList2 auditStores={this.state.qa_pending}/>;
+			return <AuditStoreTables auditStores={this.state.qa_pending}/>;
 		}
 	}
 }
