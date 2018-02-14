63bb90bbf7e886ebc1e7c49f427021bce6bfe31f
satyanash <satyajeet@webexports.co.in>
2018-02-14 22:32:19 +0530

    Bump version: 1.2.4 → 1.2.5

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import sys
 from subprocess import call
 from setuptools import setup, find_packages
 
-version = '1.2.4'
+version = '1.2.5'
 
 if sys.argv[-1] == 'build_frontend':
     os.chdir('frontend')
