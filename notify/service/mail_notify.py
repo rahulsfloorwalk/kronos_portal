f1bee4caf4a016f37709b89026657b19192bc98d
satyanash <satyajeet@webexports.co.in>
2018-02-21 19:52:31 +0530

    add store phone number to acknowledge email

--- notify/service/mail_notify.py
+++ notify/service/mail_notify.py
@@ -49,6 +49,7 @@ def notification_email_task(notif_id):
         # the audit property may come from either an application or audit_store instance
         params['client'] = notif.action_object.audit.audit_cycle.client.auditor_display_name()
         params['store_name'] = notif.action_object.audit.store.name
+        params['store_phone'] = notif.action_object.audit.store.phone
         params['store_address'] = notif.action_object.audit.store.address
 
         if notif.verb == verbs.AUDIT_APPLICATION_APPLIED:
