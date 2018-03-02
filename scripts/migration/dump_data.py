93bd6f2c33ccc31aafc37b0a11e5a710fae50d7b
argon <xamit.94@gmail.com>
2018-03-02 23:04:00 +0530

    add migration script for demo client

--- scripts/migration/dump_data.py
+++ scripts/migration/dump_data.py
@@ -11,18 +11,18 @@ def get_data_dump_for_client(client_id):
     data = {}
     client = Client.objects.get(pk=client_id)
     client_dict = {}
+
+    client_dict['id'] = client.id
     client_dict['name'] = client.name
+    client_dict['brand_name'] = client.brand_name
     client_dict['email'] = client.email
     client_dict['phone'] = client.phone
     client_dict['logo_url'] = client.logo_url
     client_dict['brand_logo_url'] = client.brand_logo_url
-    client_dict['brand_name'] = client.brand_name
-    data['client'] = client_dict
-    stores = get_stores_for_client_id(client.id)
-    audit_cycles = get_audit_cycles_for_client_id(client.id)
-    data['stores'] = stores
-    data['audit_cycles'] = audit_cycles
+    client_dict['stores'] = get_stores_for_client_id(client.id)
+    client_dict['audit_cycles'] = get_audit_cycles_for_client_id(client.id)
 
+    data['client'] = client_dict
     with open(os.path.dirname(os.path.realpath(__file__)) + '/data/dump.json', "w") as outfile:
     # with open('./data/dump.json', 'w') as outfile:
         outfile.write(json.dumps(data, indent=4))
@@ -35,15 +35,19 @@ def get_stores_for_client_id(client_id):
     stores = Store.objects.filter(client_id=client_id)
     for store in stores:
         store_dict = {}
+
+        store_dict['id'] = store.id
+        store_dict['code'] = store.code
+        store_dict['type'] = store.type
+        store_dict['priority'] = store.priority
         store_dict['name'] = store.name
         store_dict['address'] = store.address
-        store_dict['code'] = store.code
-        store_dict['priority'] = store.priority
-        store_dict['type'] = store.type
+        # store_dict['location'] = store.location.id
+        store_dict['city_id'] = store.city.id
+        store_dict['client_id'] = store.client.id
         store_dict['phone'] = store.phone
         store_dict['extra_data'] = store.extra_data
-        store_dict['city'] = store.city.id
-        store_dict['location'] = store.location.id
+
         data.append(store_dict)
     return data
 
@@ -52,18 +56,23 @@ def get_audit_cycles_for_client_id(client_id):
     audit_cycles = AuditCycle.objects.filter(client_id=client_id)
     for audit_cycle in audit_cycles:
         audit_cycle_dict = {}
+
+        audit_cycle_dict['id'] = audit_cycle.id
+        audit_cycle_dict['name'] = audit_cycle.name
         audit_cycle_dict['type'] = audit_cycle.type
         audit_cycle_dict['status'] = audit_cycle.status
         audit_cycle_dict['start_date'] = audit_cycle.start_date.strftime('%m/%d/%Y')
         audit_cycle_dict['end_date'] = audit_cycle.end_date.strftime('%m/%d/%Y')
         audit_cycle_dict['earnings_per_audit'] = audit_cycle.earnings_per_audit
-        audit_cycle_dict['description'] = audit_cycle.description
-        audit_cycle_dict['name'] = audit_cycle.name
-        audit_cycle_dict['reimbursement'] = audit_cycle.reimbursement
-        audit_cycle_dict['post_approval_description'] = audit_cycle.post_approval_description
         audit_cycle_dict['revenue_per_audit'] = audit_cycle.revenue_per_audit
+        audit_cycle_dict['reimbursement'] = audit_cycle.reimbursement
+        audit_cycle_dict['description'] = audit_cycle.description
+        audit_cycle_dict['client_id'] = audit_cycle.client.id
+        audit_cycle_dict['post_approval_description'] = audit_cycle.post_approval_description
+
         audit_cycle_dict['audits'] = get_audits_for_audit_cycle(audit_cycle.id)
         audit_cycle_dict['sections'] = get_sections_for_audit_cycle(audit_cycle.id)
+
         data.append(audit_cycle_dict)
     return data
 
@@ -72,12 +81,17 @@ def get_audits_for_audit_cycle(audit_cycle_id):
     audits = Audit.objects.filter(audit_cycle_id=audit_cycle_id)
     for audit in audits:
         audit_dict = {}
+
+        audit_dict['id'] = audit.id
         audit_dict['count'] = audit.count
         audit_dict['earnings_per_audit'] = audit.earnings_per_audit
         audit_dict['reimbursement'] = audit.reimbursement
+        audit_dict['store_id'] = audit.store.id
+        audit_dict['audit_cycle_id'] = audit.audit_cycle.id
         audit_dict['post_approval_description'] = audit.post_approval_description
         audit_dict['hidden'] = audit.hidden
-        audit_dict['reports'] = get_auditstores_for_audit(audit.id)
+        audit_dict['audit_stores'] = get_auditstores_for_audit(audit.id)
+
         data.append(audit_dict)
     return data
 
@@ -86,11 +100,16 @@ def get_auditstores_for_audit(audit_id):
     auditstores = AuditStore.objects.filter(audit_id=audit_id)
     for auditstore in auditstores:
         auditstore_dict = {}
+
+        auditstore_dict['id'] = auditstore.id
         auditstore_dict['status'] = auditstore.status
         auditstore_dict['audit_date'] = auditstore.audit_date.strftime('%m/%d/%Y')
+        auditstore_dict['qa_rating'] = auditstore.qa_rating
+        auditstore_dict['audit_id'] = auditstore.audit.id
         auditstore_dict['user_id'] = 1641
         auditstore_dict['report_sections'] = get_report_sections_for_auditstore(auditstore.id)
         auditstore_dict['answers'] = get_answers_for_auditstore(auditstore.id)
+
         data.append(auditstore_dict)
     return data
 
@@ -99,9 +118,13 @@ def get_sections_for_audit_cycle(audit_cycle_id):
     sections = Section.objects.filter(audit_cycle_id=audit_cycle_id)
     for section in sections:
         section_dict = {}
+
+        section_dict['id'] = section.id
+        section_dict['audit_cycle_id'] = section.audit_cycle.id
         section_dict['name'] = section.name
         section_dict['sequence'] = section.sequence
         section_dict['questions'] = get_questions_for_section(section.id)
+
         data.append(section_dict)
     return data
 
@@ -109,9 +132,16 @@ def get_questions_for_section(section_id):
     data = []
     questions = Question.objects.filter(section_id=section_id)
     for question in questions:
+
         question_dict = {}
-        question_dict['text'] = question.question_txt
+        question_dict['id'] = question.id
+        question_dict['question_txt'] = question.question_txt
+        question_dict['max_marks'] = question.max_marks
+        question_dict['section_id'] = question.section.id
         question_dict['sequence'] = question.sequence
+        question_dict['question_type'] = question.question_type
+        question_dict['question_data'] = question.question_data
+
         data.append(question_dict)
     return data
 
@@ -120,13 +150,15 @@ def get_report_sections_for_auditstore(audit_store_id):
     report_sections = ReportSection.objects.filter(audit_store_id=audit_store_id)
     for report_section in report_sections:
         report_section_dict = {}
+
+        report_section_dict['id'] = report_section.id
+        report_section_dict['audit_store_id'] = report_section.audit_store.id
+        report_section_dict['section_id'] = report_section.section_id
         report_section_dict['pm_comment'] = report_section.pm_comment
         report_section_dict['auditor_comment'] = report_section.auditor_comment
         report_section_dict['auditor_comment_original'] = report_section.auditor_comment_original
         report_section_dict['not_application'] = report_section.not_applicable
-        report_section_dict['section_id'] = report_section.section_id
-        report_section_dict['id'] = report_section.id
-        # report_section_dict['answers'] = get_answers_for_report_section(report_section.id)
+
         data.append(report_section_dict)
     return data
 
@@ -135,12 +167,16 @@ def get_answers_for_auditstore(auditstore_id):
     answers = Answer.objects.filter(audit_store_id=auditstore_id)
     for answer in answers:
         answer_dict = {}
-        answer_dict['answer_txt'] = answer.answer_text
+
+        answer_dict['id'] = answer.id
+        answer_dict['answer_text'] = answer.answer_text
         answer_dict['answer_txt_original'] = answer.answer_text_original
         answer_dict['answer_comment'] = answer.answer_comment
         answer_dict['marks_obtained'] = answer.marks_obtained
         answer_dict['not_applicable'] = answer.not_applicable
         answer_dict['question_id'] = answer.question.id
+        answer_dict['audit_store_id'] = answer.audit_store.id
+
         data.append(answer_dict)
     return data
 
--- scripts/migration/load_data.py
+++ scripts/migration/load_data.py
@@ -7,109 +7,146 @@ from answer.models import Answer, ReportSection
 from questionnaire.models import Question, Section
 from pprint import pprint
 
+store_id_map = {}
+audit_cycle_id_map = {}
+question_id_map = {}
+section_id_map = {}
 def load_data_dump_for_client(client_id):
     with open(os.path.dirname(os.path.realpath(__file__)) + '/data/dump.json', "r") as json_data:
-        data = json.loads(json_data)
-        stores_dict = data.get('stores', [])
-        audit_cycles_list = data.get('audit_cycles', [])
+        data = json.load(json_data)
+        stores_list = data.get('client', {}).get('stores', [])
+        audit_cycles_list = data.get('client', {}).get('audit_cycles', [])
+        load_stores(client_id, stores_list)
         load_audit_cycles(client_id, audit_cycles_list)
-        load_stores(client_id, stores_dict)
 
 def load_stores(client_id, stores):
     for store_dict in stores:
         store = Store()
-        store.client_id = client_id
-        store.location_id = store_dict.get("location", None)
-        store.phone = store_dict.get("phone", "")
         store.code = store_dict.get("code", "")
-        store.city_id = store_dict.get("city", None)
-        store.priority = store_dict.get("priority", "")
-        store.extra_data = store_dict.get("extra_data", "")
-        store.address = store_dict.get("address", "")
         store.type = store_dict.get("type", "")
+        store.priority = store_dict.get("priority", "")
         store.name = store_dict.get("name", "")
+        store.address = store_dict.get("address", "")
+        store.location_id = store_dict.get("location", None)
+        store.city_id = store_dict.get("city_id", None)
+        store.client_id = client_id
+        store.phone = store_dict.get("phone", "")
+        store.extra_data = store_dict.get("extra_data", {})
+
+        store.save()
+        old_store_id = store_dict['id']
+        store_id_map[old_store_id] = store.id
+
+
 
 def load_audit_cycles(client_id, audit_cycles):
     for audit_cycle_dict in audit_cycles:
-        audit_cycle = AuditCycle()
         start_date = datetime.datetime.strptime(audit_cycle_dict.get('start_date', ""), "%m/%d/%Y")
         end_date = datetime.datetime.strptime(audit_cycle_dict.get('end_date', ""), "%m/%d/%Y")
-        audit_cycle.client_id = client_id
+
+        audit_cycle = AuditCycle()
+        audit_cycle.name = audit_cycle_dict.get('name', "")
+        audit_cycle.type = audit_cycle_dict.get('type', "")
+        audit_cycle.status = audit_cycle_dict.get('status', "")
         audit_cycle.start_date = start_date
         audit_cycle.end_date = end_date
-        audit_cycle.description = audit_cycle_dict.get('description', "")
         audit_cycle.earnings_per_audit = audit_cycle_dict.get('earnings_per_audit', "")
-        audit_cycle.reimbursement = audit_cycle_dict.get('reimbursement', "")
-        audit_cycle.name = audit_cycle_dict.get('name', "")
-        audit_cycle.post_approval_description = audit_cycle_dict.get('post_approval_description', "")
-        audit_cycle.status = audit_cycle_dict.get('status', "")
-        audit_cycle.type = audit_cycle_dict.get('type', "")
         audit_cycle.revenue_per_audit = audit_cycle_dict.get('revenue_per_audit', "")
+        audit_cycle.reimbursement = audit_cycle_dict.get('reimbursement', "")
+        audit_cycle.description = audit_cycle_dict.get('description', "")
+        audit_cycle.client_id = client_id
+        audit_cycle.post_approval_description = audit_cycle_dict.get('post_approval_description', "")
+
         audit_cycle.save()
+        old_audit_cycle_id = audit_cycle_dict['id']
+        audit_cycle_id_map[old_audit_cycle_id] = audit_cycle.id
+
         load_sections_for_audit_cycle(audit_cycle.id, audit_cycle_dict.get('sections', []))
         load_audits_for_audit_cycle(audit_cycle.id, audit_cycle_dict.get('audits', []))
+
+def load_audits_for_audit_cycle(audit_cycle_id, audits):
+    for audit_dict in audits:
+        old_store_id = audit_dict['store_id']
+        audit = Audit()
+
+        audit.count = audit_dict.get("count", "")
+        audit.earnings_per_audit = audit_dict.get("earnings_per_audit", "")
+        audit.reimbursement = audit_dict.get("reimbursement", "")
+        audit.store_id = store_id_map[old_store_id]
+        audit.audit_cycle_id = audit_cycle_id
+        audit.post_approval_description = audit_dict.get("post_approval_description", "")
+        audit.hidden = audit_dict.get("hidden", False)
+
+        audit.save()
+        load_auditstores_for_audits(audit.id, audit_dict.get("audit_stores", []))
+
+
+def load_auditstores_for_audits(audit_id, reports):
+    for auditstore_dict in reports:
+        audit_date = datetime.datetime.strptime(auditstore_dict.get('audit_date', ""), "%m/%d/%Y")
+        auditstore = AuditStore()
+        auditstore.status = auditstore_dict.get("status", "")
+        auditstore.audit_date = audit_date
+        auditstore.qa_rating = auditstore_dict['qa_rating']
+        auditstore.audit_id = audit_id
+        auditstore.user_id = auditstore_dict.get("user_id", 1641)
+        auditstore.save()
+        load_report_sections_for_auditstore(auditstore.id, auditstore_dict.get("report_sections", []))
+        load_answers_for_auditstore(auditstore.id, auditstore_dict.get("answers", []))
 
 def load_sections_for_audit_cycle(audit_cycle_id, sections):
     for section_dict in sections:
         section = Section()
         section.name = section_dict.get("name", "")
         section.sequence = section_dict.get("sequence", 1)
+        section.audit_cycle_id = audit_cycle_id
+
         section.save()
+        old_section_id = section_dict['id']
+        section_id_map[old_section_id] = section.id
+
         load_questions_for_section(section.id, section_dict.get('questions', []))
     pass
 
 def load_questions_for_section(section_id, questions):
     for question_dict in questions:
         question = Question()
-        question.question_txt = question_dict.get("text", "")
-        question.sequence = question_dict.get("sequence", 1)
+        question.question_txt = question_dict.get("question_txt", "")
+        question.max_marks = question_dict.get("max_marks", 1)
         question.section_id = section_id
+        question.sequence = question_dict.get("sequence", 1)
+        question.question_type = question_dict.get("question_type", "")
+        question.question_data = question_dict.get("question_data", "")
+
         question.save()
-
-def load_audits_for_audit_cycle(audit_cycle_id, audits):
-    for audit_dict in audits:
-        audit = Audit()
-        audit.post_approval_description = audit_dict.get("post_approval_description", "")
-        audit.earnings_per_audit = audit_dict.get("earnings_per_audit", "")
-        audit.count = audit_dict.get("count", "")
-        audit.reimbursement = audit_dict.get("reimbursement", "")
-        audit.reports = audit_dict.get("reports", "")
-        audit.audit_cycle_id = audit_cycle_id
-        audit.save()
-        load_reports_for_audits(audit.id, audit_dict.get("reports", []))
-
-
-def load_reports_for_audits(audit_id, reports):
-    for report_dict in reports:
-        auditstore = AuditStore()
-        audit_date = datetime.datetime.strptime(report_dict.get('audit_date', ""), "%m/%d/%Y")
-        auditstore.status = report_dict.get("status", "")
-        auditstore.audit_date = audit_date
-        auditstore.user_id = report_dict.get("user_id", 1641)
-        auditstore.audit_id = audit_id
-        auditstore.save()
-        load_report_sections_for_auditstore(auditstore.id, report_dict.get("report_sections", []))
-        load_answers_for_auditstore(auditstore.id, report_dict.get("answers", []))
+        old_question_id = question_dict['id']
+        question_id_map[old_question_id] = question.id
 
 def load_report_sections_for_auditstore(auditstore_id, report_sections):
     for report_section_dict in report_sections:
+        old_section_id = report_section_dict['section_id']
+
         report_section = ReportSection()
+        report_section.audit_store_id = auditstore_id
+        report_section.section_id = section_id_map[old_section_id]
         report_section.pm_comment = report_section_dict.get("pm_comment", "")
         report_section.auditor_comment = report_section_dict.get("auditor_comment", "")
-        report_section.section_id = None #TODO - get section id here
+        report_section.auditor_comment_original = report_section_dict.get("auditor_comment_original", "")
         report_section.not_applicable = report_section_dict.get("not_application", "")
-        report_section.auditor_comment_original = report_section_dict.get("pm_comment", "")
-        report_section.audit_store_id = auditstore_id
+
         report_section.save()
 
 def load_answers_for_auditstore(auditstore_id, answers):
     for answer_dict in answers:
+        old_question_id = answer_dict['question_id']
+
         answer = Answer()
-        answer.not_applicable = answer_dict.get("not_applicable", "")
-        answer.marks_obtained = answer_dict.get("marks_obtained", "")
+        answer.answer_text = answer_dict.get("answer_text", "")
         answer.answer_text_original = answer_dict.get("answer_txt_original", "")
-        answer.answer_text = answer_dict.get("answer_txt", "")
         answer.answer_comment = answer_dict.get("answer_comment", "")
-        answer.question.id = None #TODO - get question id here
-        answer.audit_store.id = auditstore_id
+        answer.marks_obtained = answer_dict.get("marks_obtained", "")
+        answer.not_applicable = answer_dict.get("not_applicable", "")
+        answer.question_id = question_id_map[old_question_id]
+        answer.audit_store_id = auditstore_id
+
         answer.save()
