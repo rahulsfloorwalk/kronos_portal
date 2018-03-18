b9114addf53ab91f750f27a76bf9b696551254dd
argon <xamit.94@gmail.com>
2018-03-18 14:10:25 +0530

    add test for city trend performing cities

--- client_report/tests/test_city_trends.py
+++ client_report/tests/test_city_trends.py
@@ -13,12 +13,20 @@ class CityTrendsTestCase(TestCase):
 
     def setUp(self):
         self.audit_cycle_id = 1
+        self.client_user_id = 4
+        self.client_admin_id = 5
 
     def test_get_performing_cities(self):
         audit_cycle = AuditCycle.objects.get(pk=self.audit_cycle_id)
         city_trend = get_performing_cities(audit_cycle)
         self.assertEqual(len(city_trend), 7)
         self.assertTrue(city_trend[0][1].get('value') >= city_trend[1][1].get('value'))
+
+    def test_get_performing_cities_by_type_for_clientuser(self):
+        performing_cities = get_performing_cities_by_type_for_clientuser(AuditCycle.WALKIN, 4)
+        self.assertEqual(performing_cities.get('type'), AuditCycle.WALKIN)
+        self.assertEqual(len(performing_cities.get('columns')), 3)
+        self.assertEqual(len(performing_cities.get('data')), 7)
 
 
 
