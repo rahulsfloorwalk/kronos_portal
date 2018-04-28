58d50c1d8bc8f2b08713cadca55fd54d98e7f596
satyanash <satyajeet@webexports.co.in>
2018-04-28 13:07:08 +0530

    add jest tests for <Checkbox/>

---
+++ frontend/js/__tests__/components/Checkbox.test.jsx
@@ -0,0 +1,46 @@
+import React from "react";
+import { shallow } from "enzyme";
+import Checkbox from "../../components/Checkbox.jsx";
+import renderer from "react-test-renderer";
+
+describe("<Checkbox/>", () => {
+	it("is checked", () => {
+		const tree = renderer
+			.create(<Checkbox checked={true}/>)
+			.toJSON();
+		expect(tree).toMatchSnapshot();
+	});
+	it("is unchecked", () => {
+		const tree = renderer
+			.create(<Checkbox checked={false}/>)
+			.toJSON();
+		expect(tree).toMatchSnapshot();
+	});
+	it("is disabled", () => {
+		const tree = renderer
+			.create(<Checkbox checked={true} disabled={true}/>)
+			.toJSON();
+		expect(tree).toMatchSnapshot();
+	});
+
+	it("calls onChange with true when checked", () => {
+		const onChangeCb = jest.fn();
+		const w = shallow(<Checkbox checked={false} onChange={onChangeCb}/>);
+		w.find("button").simulate("click");
+		expect(onChangeCb).lastCalledWith(true);
+	});
+
+	it("calls onChange with false when unchecked", () => {
+		const onChangeCb = jest.fn();
+		const w = shallow(<Checkbox checked={true} onChange={onChangeCb}/>);
+		w.find("button").simulate("click");
+		expect(onChangeCb).lastCalledWith(false);
+	});
+
+	it("does not call onChange when disabled", () => {
+		const onChangeCb = jest.fn();
+		const w = shallow(<Checkbox checked={true} onChange={onChangeCb} disabled={true}/>);
+		w.simulate("click");
+		expect(onChangeCb).not.toBeCalled();
+	});
+});
