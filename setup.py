20a362e24dcb8d676aa16f6c0e5652006cd9386c
argon <xamit.94@gmail.com>
2018-05-15 23:57:20 +0530

    Bump version: 1.7.0 → 1.7.1

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import sys
 from subprocess import call
 from setuptools import setup, find_packages
 
-version = '1.7.0'
+version = '1.7.1'
 
 if sys.argv[-1] == 'build_frontend':
     os.chdir('frontend')
