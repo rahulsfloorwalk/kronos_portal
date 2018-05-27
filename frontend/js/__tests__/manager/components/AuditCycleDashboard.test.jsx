fa150df1c878d38ccd7401d96f6d3845270c839a
satyanash <satyajeet@webexports.co.in>
2018-05-27 14:39:38 +0530

    +Gaiki | Fix manager dashboard to show PM Review status

---
+++ frontend/js/__tests__/manager/components/AuditCycleDashboard.test.jsx
@@ -0,0 +1,73 @@
+import React from "react";
+import { shallow } from "enzyme";
+import AuditCycleDashboard from "../../../manager/components/AuditCycleDashboard.jsx";
+import renderer from "react-test-renderer";
+import { getDashboardAuditCycles } from "../../../manager/service/dashboard_audit_cycles";
+
+jest.mock("../../../manager/service/dashboard_audit_cycles");
+
+describe("<AuditCycleDashboard/>", () => {
+	const sampleStats = [
+		{
+			"id": 133,
+			"name": "Fine Dine April 2018",
+			"status": "REPORT",
+			"client": "Smaaash",
+			"start_date": "2018-04-06",
+			"end_date": "2018-04-15",
+			"audit_count": 1,
+			"stats": {
+				"application": {
+					"APPLIED": 32,
+					"APPROVED": 1,
+					"NOT_APPLIED": 0,
+					"REJECTED": 0,
+					"WAITLISTED": 0
+				},
+				"audit_store": {
+					"ACCEPTED": 1,
+					"ASSIGNED": 0,
+					"FAILED": 0,
+					"ACKNOWLEDGED": 0,
+					"SUBMITTED": 0,
+					"PM_REVIEW": 0,
+					"COMPLETED": 0,
+					"WITHDRAWN": 0,
+					"REJECTED": 0
+				}
+			}
+		},
+	];
+
+	it("is rendered correctly when the data are loading", () => {
+		getDashboardAuditCycles.mockResolvedValue(sampleStats);
+		const r = renderer.create(<AuditCycleDashboard/>);
+		expect(r.toJSON()).toMatchSnapshot();
+	});
+
+	it("is rendered correctly when the data is successfully loaded", (done) => {
+		getDashboardAuditCycles.mockResolvedValue(sampleStats);
+		const r = renderer.create(<AuditCycleDashboard/>);
+		setTimeout(() => {
+			expect(r.toJSON()).toMatchSnapshot();
+			done();
+		});
+	});
+
+	it("calls the stats service correctly", () => {
+		getDashboardAuditCycles.mockResolvedValue(sampleStats);
+		const r = shallow(<AuditCycleDashboard/>);
+		expect(getDashboardAuditCycles).toBeCalled();
+	});
+
+	it("sets the state correctly once data is loaded", (done) => {
+		getDashboardAuditCycles.mockResolvedValue(sampleStats);
+		const r = shallow(<AuditCycleDashboard/>);
+		setTimeout(() => {
+			expect(r.state()).toEqual({
+				"active_cycles": sampleStats
+			});
+			done();
+		});
+	});
+});
--- frontend/js/manager/components/AuditCycleDashboard.jsx
+++ frontend/js/manager/components/AuditCycleDashboard.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import * as ReactRedux from 'react-redux';
 import { hashHistory, Link } from 'react-router';
 
@@ -21,45 +21,45 @@ import Panel from '../../components/Panel.jsx';
 // import Jumbotron from '../Jumbotron.jsx'
 import Loading from '../../components/Loading.jsx';
 
-var AuditCycleDashBoard = React.createClass({
-
-  getInitialState: function(){
-    return {
-    };
-  },
-	componentDidMount: function(){
-    getDashboardAuditCycles().then((active_cycles)=> this.setState({
+export default class AuditCycleDashBoard extends Component{
+	constructor(props){
+		super(props);
+		this.state = {};
+	}
+	componentDidMount(){
+		getDashboardAuditCycles().then((active_cycles)=> this.setState({
 			active_cycles
 		}));
-	},
+	}
 	componentWillReceiveProps(nextProps){
 		console.log("AuditCycleSummary#componentWillReceiveProps#nextProps", nextProps);
-	},
-	render: function(){
+	}
+	render(){
 		if(! this.state.active_cycles){
 			return <Loading/>;
 		}
-    let audit_cycle_blocks = this.state.active_cycles.map((value) => {
-      let linkTo = `/audit_cycle/${value.id}/questionnaire`
-      return (
-          <tr key={value.id} onClick={()=> hashHistory.push(linkTo)} style={{cursor:'pointer'}} title="Click to open Audit Cycle">
-	      <td className="">
-	      <small>
-	      <b>{value.name}</b><br/>
-	      <b>{value.client}</b> - {getAuditStatus(value.status)}</small>
-	      </td>
-	      <td className="text-right"><b>{value.stats.application.APPLIED || ""}</b></td>
-	      <td className="text-right"><b>{value.stats.application.WAITLISTED || ""}</b></td>
-	      <td className="text-right"><b>{value.stats.application.APPROVED || ""}</b></td>
-	      <td className="text-right"><b>{value.stats.audit_store.ASSIGNED || ""}</b></td>
-	      <td className="text-right"><b>{value.stats.audit_store.ACKNOWLEDGED || ""}</b></td>
-	      <td className="text-right"><b>{value.stats.audit_store.SUBMITTED || ""}</b></td>
-	      <td className="text-right"><b>{value.stats.audit_store.COMPLETED || ""}</b></td>
-	      <td className="text-right"><b>{value.stats.audit_store.ACCEPTED || ""}</b></td>
-	      <td className="text-right"><b>{value.audit_count}</b></td>
-          </tr>
-      );
-    });
+		let audit_cycle_blocks = this.state.active_cycles.map((value) => {
+			let linkTo = `/audit_cycle/${value.id}/questionnaire`
+			return (
+				<tr key={value.id} onClick={()=> hashHistory.push(linkTo)} style={{cursor:'pointer'}} title="Click to open Audit Cycle">
+					<td className="">
+					<small>
+					<b>{value.name}</b><br/>
+					<b>{value.client}</b> - {getAuditStatus(value.status)}</small>
+					</td>
+					<td className="text-right"><b>{value.stats.application.APPLIED || ""}</b></td>
+					<td className="text-right"><b>{value.stats.application.WAITLISTED || ""}</b></td>
+					<td className="text-right"><b>{value.stats.application.APPROVED || ""}</b></td>
+					<td className="text-right"><b>{value.stats.audit_store.ASSIGNED || ""}</b></td>
+					<td className="text-right"><b>{value.stats.audit_store.ACKNOWLEDGED || ""}</b></td>
+					<td className="text-right"><b>{value.stats.audit_store.SUBMITTED || ""}</b></td>
+					<td className="text-right"><b>{value.stats.audit_store.PM_REVIEW || ""}</b></td>
+					<td className="text-right"><b>{value.stats.audit_store.COMPLETED || ""}</b></td>
+					<td className="text-right"><b>{value.stats.audit_store.ACCEPTED || ""}</b></td>
+					<td className="text-right"><b>{value.audit_count}</b></td>
+				</tr>
+			);
+		});
 		return (
 			<div className="table-responsive">
 			<table className="table table-hover table-striped table-bordered table-condensed">
@@ -67,7 +67,7 @@ var AuditCycleDashBoard = React.createClass({
 					<tr>
 					      <th rowSpan="2">Client</th>
 					      <th colSpan="3" className="text-center">Application Status</th>
-					      <th colSpan="5" className="text-center">Report Status</th>
+					      <th colSpan="6" className="text-center">Report Status</th>
 					      <th rowSpan="2" className="text-right">Planned Audits</th>
 					</tr>
 					<tr>
@@ -77,6 +77,7 @@ var AuditCycleDashBoard = React.createClass({
 					      <th className="text-right"><AuditStoreStatusLabel status={"ASSIGNED"}/></th>
 					      <th className="text-right"><AuditStoreStatusLabel status={"ACKNOWLEDGED"}/></th>
 					      <th className="text-right"><AuditStoreStatusLabel status={"SUBMITTED"}/></th>
+					      <th className="text-right"><AuditStoreStatusLabel status={"PM_REVIEW"}/></th>
 					      <th className="text-right"><AuditStoreStatusLabel status={"COMPLETED"}/></th>
 					      <th className="text-right"><AuditStoreStatusLabel status={"ACCEPTED"}/></th>
 					</tr>
@@ -87,7 +88,6 @@ var AuditCycleDashBoard = React.createClass({
 			</table>
 			</div>
 		);
-	},
-});
+	}
+}
 
-export default AuditCycleDashBoard;
