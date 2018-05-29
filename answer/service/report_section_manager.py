92b98a16f3de6b98f08183441413726cfb8eaca0
satyanash <satyajeet@webexports.co.in>
2018-05-30 00:03:29 +0530

    Use set_pm_comment model method in set_pm_comment_for_manager

--- answer/service/report_section_manager.py
+++ answer/service/report_section_manager.py
@@ -18,3 +18,11 @@ def set_auditor_comment_for_manager(audit_store_id, section_id, auditor_comment)
         return report_section
     else:
         raise AppLogicError("Report Section auditor comment cannot be set now")
+
+def set_pm_comment_for_manager(audit_store_id, section_id, pm_comment):
+    report_section = report_section_service.find_by_audit_store_and_section(audit_store_id, section_id)
+    if report_section.audit_store.is_editable_by_manager():
+        report_section.set_pm_comment(pm_comment)
+        return report_section
+    else:
+        raise AppLogicError("Report Section pm comment cannot be set now")
--- answer/tests/test_report_section_manager_service.py
+++ answer/tests/test_report_section_manager_service.py
@@ -67,3 +67,25 @@ class ReportSectionManagerServiceTestCase(TestCase):
                         auditor_comment,
                     )
 
+    def test_set_pm_comment_for_manager_sets_pm_comment_or_raises(self):
+        audit_store_recipe = Recipe(AuditStore, user=self.auditor_user)
+        for status in [s[0] for s in AuditStore.STATUS]:
+            audit_cycle = mommy.make(AuditCycle, status=AuditCycle.ACTIVE)
+            audit_store = audit_store_recipe.make(status=status, audit__audit_cycle=audit_cycle)
+            report_section = mommy.make(ReportSection, audit_store=audit_store, section__audit_cycle=audit_cycle)
+            pm_comment = "Hello World"
+            if audit_store.status in audit_store._MANAGER_EDITABLE_STATUSES:
+                saved_report_section = report_section_manager_service.set_pm_comment_for_manager(
+                    audit_store.id,
+                    report_section.section.id,
+                    pm_comment,
+                )
+                self.assertEqual(pm_comment, saved_report_section.pm_comment)
+            else:
+                with self.assertRaisesRegex(AppLogicError, "Report Section pm comment cannot be set now"):
+                    report_section_manager_service.set_pm_comment_for_manager(
+                        audit_store.id,
+                        report_section.section.id,
+                        pm_comment,
+                    )
+
