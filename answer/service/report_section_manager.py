9eef7dc87d819ce5a266ce282978da9019dd49a6
satyanash <satyajeet@webexports.co.in>
2018-05-29 22:22:58 +0530

    Use set_not_applicable model method in set_not_applicable_for_manager

---
+++ answer/service/report_section_manager.py
@@ -0,0 +1,12 @@
+from kronos.exceptions import AppLogicError
+from audit_store import service
+from answer.service import report_section as report_section_service
+
+
+def set_not_applicable_for_manager(audit_store_id, section_id, not_applicable):
+    report_section = report_section_service.find_by_audit_store_and_section(audit_store_id, section_id)
+    if report_section.audit_store.is_editable_by_manager():
+        report_section.set_not_applicable(not_applicable)
+        return report_section
+    else:
+        raise AppLogicError("Report Section not applicable cannot be set now")
---
+++ answer/tests/test_report_section_manager_service.py
@@ -0,0 +1,47 @@
+from django.test import TestCase
+from django.contrib.auth.models import User, Group
+
+from model_mommy import mommy
+from model_mommy.recipe import Recipe
+
+from kronos.exceptions import AppLogicError
+from answer.models import ReportSection
+from answer.service import report_section_manager as report_section_manager_service
+from audit_store.models import AuditStore
+from audit.models import AuditCycle
+from registration.models import GROUP_NAME_AUDITOR, GROUP_NAME_MANAGER
+from auditor.models import ProfileInfo
+
+class ReportSectionManagerServiceTestCase(TestCase):
+    fixtures = ['groups', 'city']
+
+    def setUp(self):
+        self.auditor_group = Group.objects.get(name=GROUP_NAME_AUDITOR)
+        self.manager_group = Group.objects.get(name=GROUP_NAME_MANAGER)
+        self.manager_user = mommy.make(User, username="manager@foobar.com", email="manager@foobar.com",
+                                       groups=[self.manager_group])
+        self.auditor_user = mommy.make(User, username="auditor@foobar.com", email="auditor@foobar.com",
+                                       groups=[self.auditor_group])
+        self.auditor_profile = mommy.make(ProfileInfo, user=self.auditor_user)
+
+    def test_set_not_applicable_for_manager_sets_not_applicable(self):
+        audit_store_recipe = Recipe(AuditStore, user=self.auditor_user)
+        for status in [s[0] for s in AuditStore.STATUS]:
+            audit_cycle = mommy.make(AuditCycle, status=AuditCycle.ACTIVE)
+            audit_store = audit_store_recipe.make(status=status, audit__audit_cycle=audit_cycle)
+            report_section = mommy.make(ReportSection, audit_store=audit_store, section__audit_cycle=audit_cycle)
+            if audit_store.status in audit_store._MANAGER_EDITABLE_STATUSES:
+                saved_report_section = report_section_manager_service.set_not_applicable_for_manager(
+                    audit_store.id,
+                    report_section.section.id,
+                    True
+                )
+                self.assertTrue(saved_report_section.not_applicable)
+            else:
+                with self.assertRaises(AppLogicError):
+                    report_section_manager_service.set_not_applicable_for_manager(
+                        audit_store.id,
+                        report_section.section.id,
+                        True
+                    )
+
