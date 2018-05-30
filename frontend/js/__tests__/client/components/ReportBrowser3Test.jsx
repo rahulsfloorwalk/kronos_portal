85aae393e238f78c33c62172684010b8f2b138e0
satyanash <satyajeet@webexports.co.in>
2018-05-30 23:42:41 +0530

    +Gaiki | Add tests for ReportBrowser3

--- frontend/js/__tests__/client/components/ReportBrowser3Test.jsx
+++ frontend/js/__tests__/client/components/ReportBrowser3Test.jsx
@@ -3,30 +3,62 @@ import Datetime from 'react-datetime';
 import { shallow } from "enzyme";
 import renderer from "react-test-renderer";
 
-import { fetchAuditCycles } from '../../../client/service/audit_cycle.js';
+import ReportBrowser3 from '../../../client/components/ReportBrowser3';
+import { fetchAuditCycles } from '../../../client/service/audit_cycle';
 
 jest.mock("react-dom", () => ({
 	findDOMNode: () => {},
 }));
 
-jest.mock('../../../client/service/audit_cycle.js');
+jest.mock('../../../client/service/audit_cycle');
 
 describe("<ReportBrowser3/>", () => {
-  const sampleAuditCycles = [
-    {
-      "audit__audit_cycle__id":109,
-      "audit__audit_cycle__end_date":"2018-05-31",
-      "audit__audit_cycle__start_date":"2018-03-12",
-      "audit__audit_cycle__name":"Wave-1- 2018",
-      "audit__audit_cycle__type":"WALKIN"
-    }
-  ];
+	const sampleAuditCycles = [
+		{
+			"audit__audit_cycle__id":109,
+			"audit__audit_cycle__start_date":"2018-03-12",
+			"audit__audit_cycle__end_date":"2018-05-31",
+			"audit__audit_cycle__name":"Wave-1- 2018",
+			"audit__audit_cycle__type":"WALKIN",
+		},
+		{
+			"audit__audit_cycle__id":129,
+			"audit__audit_cycle__start_date":"2018-03-12",
+			"audit__audit_cycle__end_date":"2018-05-31",
+			"audit__audit_cycle__name":"Wave-1- 2018",
+			"audit__audit_cycle__type":"WALKIN",
+		},
+	];
+
 	it("selects first audit cycle on successfully loading audit cycle list", (done) => {
-    fetchAuditCycles.mockResolvedValue(sampleAuditCycles);
-    const r = shallow(<ReportBrowser3/>);
-    setTimeout( () => {
-      expect(r.find("select").value).toEqual(sampleAuditCycles[0].audit__audit_cycle__id);
-      done();
-    });
+		fetchAuditCycles.mockResolvedValue(sampleAuditCycles);
+		const r = shallow(<ReportBrowser3/>);
+		setTimeout(() => {
+			r.update();
+			expect(r.find("select > option").length).toEqual(2);
+			expect(r.find("select").prop("value")).toEqual(sampleAuditCycles[0].audit__audit_cycle__id);
+			done();
+		});
+	});
+
+	it("passes the id, startDate and endDate of the selected audit cycle to AuditStoreTable", (done) => {
+		fetchAuditCycles.mockResolvedValue(sampleAuditCycles);
+		const r = shallow(<ReportBrowser3/>);
+		setTimeout(() => {
+			r.update();
+			expect(r.find("AuditStoreTable").prop("auditCycleId")).toEqual(sampleAuditCycles[0].audit__audit_cycle__id);
+			expect(r.find("AuditStoreTable").prop("startDate")).toEqual(sampleAuditCycles[0].audit__audit_cycle__start_date);
+			expect(r.find("AuditStoreTable").prop("endDate")).toEqual(sampleAuditCycles[0].audit__audit_cycle__end_date);
+			r.find("select").simulate("change", {
+				target: {
+					value: r.find("option").at(1).prop("value"),
+				},
+			});
+			r.update();
+			expect(r.find("AuditStoreTable").prop("auditCycleId")).toEqual(sampleAuditCycles[1].audit__audit_cycle__id);
+			expect(r.find("AuditStoreTable").prop("startDate")).toEqual(sampleAuditCycles[1].audit__audit_cycle__start_date);
+			expect(r.find("AuditStoreTable").prop("endDate")).toEqual(sampleAuditCycles[1].audit__audit_cycle__end_date);
+			done();
+		});
 	});
 });
