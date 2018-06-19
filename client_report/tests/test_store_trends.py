1d1bca01fedae63098c92fa057409a546ca890cc
satyanash <satyajeet@webexports.co.in>
2018-06-20 00:00:35 +0530

    Change store trends to use questionnaire type id instead of audit type

--- client_report/tests/test_store_trends.py
+++ client_report/tests/test_store_trends.py
@@ -4,7 +4,7 @@ from django.test import TestCase
 from faker import Faker
 
 from audit.models import AuditCycle
-from client_report.service.store_trends import *
+from client_report.service import store_trends
 
 fake = Faker()
 
@@ -14,20 +14,17 @@ class StoreTrendsTestCase(TestCase):
     def setUp(self):
         self.audit_cycle_id = 1
         self.client_admin_id = 5
+        self.questionnaire_type_id = 1
 
     def test_get_performing_stores(self):
         audit_cycle = AuditCycle.objects.get(pk=self.audit_cycle_id)
-        store_trend = get_performing_stores(audit_cycle)
+        store_trend = store_trends.get_performing_stores(audit_cycle)
         self.assertEqual(len(store_trend), 11)
-        self.assertTrue(store_trend[0][1].get('value') >= store_trend[1][1].get('value'))
+        self.assertTrue(store_trend[0][1]['value'] >= store_trend[1][1]['value'])
 
     def test_get_performing_cities_by_type_for_clientuser(self):
-        performing_stores = get_performing_stores_by_type_for_clientuser(AuditCycle.WALKIN, self.client_admin_id)
-        self.assertEqual(performing_stores.get('type'), AuditCycle.WALKIN)
-        self.assertEqual(len(performing_stores.get('columns')), 3)
-        self.assertEqual(len(performing_stores.get('data')), 11)
-
-
-
-
+        performing_stores = store_trends.get_performing_stores_by_type_for_clientuser(self.questionnaire_type_id, self.client_admin_id)
+        self.assertEqual(performing_stores['type'], self.questionnaire_type_id)
+        self.assertEqual(len(performing_stores['columns']), 3)
+        self.assertEqual(len(performing_stores['data']), 11)
 
