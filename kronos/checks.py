516196810a9acba2736394306f14452a73031c6f
satyanash <satyajeet@webexports.co.in>
2018-06-28 21:27:37 +0530

    Fix checks to ignore new dirs, add __init__.py to the rest

--- kronos/checks.py
+++ kronos/checks.py
@@ -7,7 +7,7 @@ from django.apps import apps
 from django.db.models import ForeignKey, OneToOneField
 from django.db.models import CASCADE
 
-ignore_dirs = re.compile("^.*(__pycache__|\.git|frontend|venv|datasets|templates|fixtures|egg-info|dist|static|logs|htmlcov)$")
+ignore_dirs = re.compile("^.*(__pycache__|.git|frontend|venv|datasets|templates|fixtures|egg-info|dist|static|logs|htmlcov|.circleci|test_data|scripts/data)$")
 
 def check_dir(directory):
     messages = []
