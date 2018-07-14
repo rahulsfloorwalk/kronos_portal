e0d651f9a09f811ed1613febea18eb2d2d7a5757
satyanash <satyajeet@webexports.co.in>
2018-07-14 23:29:21 +0530

    Green Highlight auditor comment if it is non empty in Agency UI

--- frontend/js/agency/components/audit_store/Section.jsx
+++ frontend/js/agency/components/audit_store/Section.jsx
@@ -25,60 +25,61 @@ export class __Section extends React.Component{
 
 		auditStore: auditStorePropType,
 		showErrors: PropTypes.bool,
+		auditorComment: PropTypes.string,
 	};
 
 	state = {};
 
 	render(){
 		if(this.props.auditStore && this.props.section){
-		const questionRows = [];
-		if( this.props.section.questions){
-			for(let q of this.props.section.questions){
-				questionRows.push(<QuestionRow auditStoreId={this.props.auditStore.id} question={q} key={q.id} showErrors={this.props.showErrors}/>);
+			const questionRows = [];
+			if( this.props.section.questions){
+				for(let q of this.props.section.questions){
+					questionRows.push(<QuestionRow auditStoreId={this.props.auditStore.id} question={q} key={q.id} showErrors={this.props.showErrors}/>);
+				}
 			}
-		}
-		if(questionRows.length === 0){
-			questionRows.push(<tr key="empty"><td className="text-center text-muted">no questions here</td></tr>);
-		}
+			if(questionRows.length === 0){
+				questionRows.push(<tr key="empty"><td className="text-center text-muted">no questions here</td></tr>);
+			}
 
-		let goodClass = this.state.focused || this.state.saving || !this.state.auditor_comment ? "" : "success";
-		let badClass = this.props.showErrors && !this.state.auditor_comment ? "danger" : "";
+			const goodClass = this.props.auditorComment ? "success" : "";
+			const badClass = this.props.showErrors && !this.props.auditorComment ? "danger" : "";
 
-		return (
-			<div className="panel panel-default">
-				<div className="panel-heading">
-					<h4 className="panel-title">
-						{this.props.section.sequence} - <b>{this.props.section.name}</b>
-					</h4>
-				</div>
-				<table className="table table-striped">
-					<thead>
-						<tr>
-							<th>
-								<div className="row">
-									<div className="col-xs-1 text-right">#</div>
-									<div className="col-xs-10 col-md-5">Question</div>
-									<div className="col-xs-12 col-md-6 hidden-xs hidden-sm">Answer</div>
-								</div>
-							</th>
-						</tr>
-					</thead>
-					<tbody>
-						{questionRows}
-						<tr className={goodClass || badClass}>
-							<td>
-								<div className="row">
-									<div className="col-xs-offset-1 col-md-11">
-										<AuditorComment auditStoreId={this.props.auditStore.id} sectionId={this.props.section.id}/>
+			return (
+				<div className="panel panel-default">
+					<div className="panel-heading">
+						<h4 className="panel-title">
+							{this.props.section.sequence} - <b>{this.props.section.name}</b>
+						</h4>
+					</div>
+					<table className="table table-striped">
+						<thead>
+							<tr>
+								<th>
+									<div className="row">
+										<div className="col-xs-1 text-right">#</div>
+										<div className="col-xs-10 col-md-5">Question</div>
+										<div className="col-xs-12 col-md-6 hidden-xs hidden-sm">Answer</div>
 									</div>
-								</div>
-							</td>
-						</tr>
-					</tbody>
-				</table>
-				<SectionAttachmentBox auditStoreId={this.props.auditStore.id} sectionId={this.props.section.id} auditStore={this.props.auditStore}/>
-			</div>
-		);
+								</th>
+							</tr>
+						</thead>
+						<tbody>
+							{questionRows}
+							<tr className={goodClass || badClass}>
+								<td>
+									<div className="row">
+										<div className="col-xs-offset-1 col-md-11">
+											<AuditorComment auditStoreId={this.props.auditStore.id} sectionId={this.props.section.id}/>
+										</div>
+									</div>
+								</td>
+							</tr>
+						</tbody>
+					</table>
+					<SectionAttachmentBox auditStoreId={this.props.auditStore.id} sectionId={this.props.section.id} auditStore={this.props.auditStore}/>
+				</div>
+			);
 		} else {
 			return null;
 		}
@@ -93,11 +94,17 @@ const findSection = (store, sectionId) => {
 	return store.sections.find((s) => s.id === sectionId);
 };
 
+const findReportSection = (store, auditStoreId, sectionId) => {
+	return store.reportSections.find((rs) => rs.audit_store_id === auditStoreId && rs.section_id === sectionId);
+};
+
 const mapStateToProps = (store, ownProps) => {
+	const reportSection = findReportSection(store, ownProps.auditStoreId, ownProps.sectionId);
 	return {
 		auditStore: findAuditStore( store, ownProps.auditStoreId),
 		section: findSection(store, ownProps.sectionId),
 		showErrors: ownProps.showErrors,
+		auditorComment: reportSection && reportSection.auditor_comment,
 	};
 };
 
