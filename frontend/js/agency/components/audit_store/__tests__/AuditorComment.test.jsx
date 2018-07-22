1ad10f5b68001e91a2f7e0ce7e3bc83ca915bb2a
satyanash <satyajeet@webexports.co.in>
2018-07-22 15:58:39 +0530

    add tests for Agency UI AuditorComment

---
+++ frontend/js/agency/components/audit_store/__tests__/AuditorComment.test.jsx
@@ -0,0 +1,44 @@
+import React from "react";
+import renderer from "react-test-renderer";
+import { shallow } from "enzyme";
+
+import { __AuditorComment } from "../AuditorComment.jsx";
+
+describe("<__AuditorComment/>", () => {
+	describe("when auditor comment is editable", () => {
+		const editable = true;
+
+		it("renders a comment correctly", () => {
+			const tree = renderer.create(<__AuditorComment auditorComment="Hello World" onCommentChanged={jest.fn()} editable={editable}/>).toJSON();
+			expect(tree).toMatchSnapshot();
+		});
+
+		it("calls onCommentChanged when input is blurred and has changed", () => {
+			const onCommentChanged = jest.fn();
+			const sampleComment = "Hello World";
+			const r = shallow(<__AuditorComment auditorComment="" onCommentChanged={onCommentChanged} editable={editable}/>);
+			r.find("input").simulate("change", { target: { value: sampleComment}});
+			r.find("input").simulate("blur");
+			expect(onCommentChanged).toHaveBeenCalledWith(sampleComment);
+		});
+
+		it("does not call onCommentChanged when input is blurred and has not changed", () => {
+			const onCommentChanged = jest.fn();
+			const sampleComment = "Hello World";
+			const r = shallow(<__AuditorComment auditorComment={sampleComment} onCommentChanged={onCommentChanged} editable={editable}/>);
+			r.find("input").simulate("change", { target: { value: sampleComment}});
+			r.find("input").simulate("blur");
+			expect(onCommentChanged).not.toHaveBeenCalled();
+		});
+	});
+
+	describe("when auditor comment is not editable", () => {
+		const editable = false;
+
+		it("renders a comment correctly", () => {
+			const tree = renderer.create(<__AuditorComment auditorComment="Hello World" onCommentChanged={jest.fn()} editable={editable}/>).toJSON();
+			expect(tree).toMatchSnapshot();
+		});
+	});
+
+});
