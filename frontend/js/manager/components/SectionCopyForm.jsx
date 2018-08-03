59e947e8b521f28a41219c5d794db7b21b4708a7
satyanash <satyajeet@webexports.co.in>
2018-08-03 22:01:04 +0530

    refactor manager SectionCopyForm and add tests

--- frontend/js/manager/components/SectionCopyForm.jsx
+++ frontend/js/manager/components/SectionCopyForm.jsx
@@ -1,44 +1,57 @@
-import React from 'react';
-import * as ReactRedux from 'react-redux';
-import { hashHistory } from 'react-router';
+import React from "react";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import { hashHistory } from "react-router";
 
-import { fetchAuditCycles } from '../actions/audit.js';
+import { fetchAuditCycles } from "../actions/audit.js";
 
-import { copySectionsFromTo } from '../service/section.js';
+import { copySectionsFromTo } from "../service/section.js";
 
-import { affectInputEventToComponent } from '../../react_utils.js';
-import FormSelect from '../../components/FormSelect.jsx';
-import FormGroup from '../../components/FormGroup.jsx';
-import SaveButton from '../../components/SaveButton.jsx';
-import Modal from '../../components/Modal.jsx';
-import { Duplicate } from '../../components/Icons.jsx';
+import { affectInputEventToComponent } from "../../react_utils.js";
+import FormSelect from "../../components/FormSelect.jsx";
+import Modal from "../../components/Modal.jsx";
+import { Duplicate } from "../../components/Icons.jsx";
 
-var SectionCopyForm = React.createClass({
-	getInitialState: function(){
-		return {
-			selectedAuditCycleId: null
-		};
-	},
-	componentDidMount: function() {
+const auditCycleProp = PropTypes.shape({
+	id: PropTypes.number.isRequired,
+	name: PropTypes.string.isRequired,
+	client: PropTypes.shape({
+		id: PropTypes.number.isRequired,
+		name: PropTypes.string.isRequired,
+	}).isRequired,
+});
+
+
+export class __SectionCopyForm extends React.Component {
+	static propTypes = {
+		auditCycle: auditCycleProp,
+		otherAuditCycles: PropTypes.arrayOf(auditCycleProp),
+		fetchAuditCycles: PropTypes.func.isRequired,
+	};
+
+	state = {
+		selectedAuditCycleId: "",
+	};
+
+	componentDidMount() {
 		if(this.props.auditCycle){
-			this.props.dispatch(fetchAuditCycles(this.props.auditCycle.client.id));
+			this.props.fetchAuditCycles(this.props.auditCycle.client.id);
 		}
-	},
-	inputChanged: function(e){
+	}
+
+	inputChanged = (e) => {
 		affectInputEventToComponent(e, this);
-	},
-	onSubmit: function(e){
+	};
+
+	onSubmit = (e) => {
 		e.preventDefault();
-		copySectionsFromTo(this.state.selectedAuditCycleId, this.props.params.auditCycleId).then((sections) => {
-			hashHistory.push(`/audit_cycle/${this.props.params.auditCycleId}/questionnaire`);
+		copySectionsFromTo(this.state.selectedAuditCycleId, this.props.auditCycle.id).then(() => {
+			hashHistory.push(`/audit_cycle/${this.props.auditCycle.id}/questionnaire`);
 		});
-	},
-	render : function(){
-		console.log("otherAuditCycles", this.props);
-		let auditCycleOptions = [];
-		for( let ac of this.props.otherAuditCycles){
-			auditCycleOptions.push(<option key={ac.id} value={ac.id}>{ac.name}</option>);
-		}
+	};
+
+	render() {
+		const auditCycleOptions = this.props.otherAuditCycles.map(ac => <option key={ac.id} value={ac.id}>{ac.name}</option>);
 		return (
 			<Modal modalTitle="Copy Sections" onClose={hashHistory.goBack}>
 				<form onSubmit={this.onSubmit}>
@@ -52,24 +65,30 @@ var SectionCopyForm = React.createClass({
 				</form>
 			</Modal>
 		);
-	},
-});
+	}
+}
 
-var mapStoreToProps = function(store, ownProps){
+const mapStoreToProps = (store, ownProps) => {
 	return {
 		auditCycle: store.auditCycles[ownProps.params.auditCycleId],
-		otherAuditCycles: (function(auditCycles){
+		otherAuditCycles: ((auditCycles) => {
 			let selectedCycles = [];
-			for( let id in auditCycles){
-				console.log("auditCycles[id].id",auditCycles[id].id);
-				console.log("ownProps.params.auditCycleId", parseInt(ownProps.params.auditCycleId));
+			for( const id in auditCycles){
 				if( auditCycles[id].id !== parseInt(ownProps.params.auditCycleId)){
 					selectedCycles.push(auditCycles[id]);
 				}
 			}
 			return selectedCycles;
-		}(store.auditCycles))
+		})(store.auditCycles)
 	};
 };
 
-export default ReactRedux.connect( mapStoreToProps)(SectionCopyForm);
+const mapDispatchToProps = (dispatch) => {
+	return {
+		fetchAuditCycles: (clientId) => {
+			dispatch(fetchAuditCycles(clientId));
+		},
+	};
+};
+
+export default connect(mapStoreToProps, mapDispatchToProps)(__SectionCopyForm);
---
+++ frontend/js/manager/components/__tests__/SectionCopyForm.test.jsx
@@ -0,0 +1,78 @@
+import React from "react";
+import renderer from "react-test-renderer";
+import { shallow } from "enzyme";
+
+import { __SectionCopyForm } from "../SectionCopyForm.jsx";
+import { copySectionsFromTo } from "../../service/section.js";
+
+jest.mock("../../service/section.js");
+
+const sampleAuditCycle = {
+	id: 1,
+	name: "Audit Cycle 1",
+	client: {
+		id: 2,
+		name: "Foobar Client",
+	},
+};
+
+const sampleAuditCycles = [
+	{
+		id: 2,
+		name: "Audit Cycle 2",
+		client: {
+			id: 2,
+			name: "Foobar Client",
+		},
+	},
+	{
+		id: 3,
+		name: "Audit Cycle 3",
+		client: {
+			id: 2,
+			name: "Foobar Client",
+		},
+	},
+];
+
+describe("<__SectionCopyForm/>", () => {
+	it("renders the list of audit cycles", () => {
+		const fetchAuditCycles = jest.fn();
+		const r = renderer.create(<__SectionCopyForm auditCycle={sampleAuditCycle} otherAuditCycles={sampleAuditCycles} fetchAuditCycles={fetchAuditCycles}/>);
+		expect(r.toJSON()).toMatchSnapshot();
+	});
+
+	it("calls fetchAuditCycles propw when the form is loaded", () => {
+		const fetchAuditCycles = jest.fn();
+		copySectionsFromTo.mockResolvedValue([]);
+
+		shallow(<__SectionCopyForm auditCycle={sampleAuditCycle} otherAuditCycles={sampleAuditCycles} fetchAuditCycles={fetchAuditCycles}/>);
+
+		expect(fetchAuditCycles).toBeCalled();
+	});
+
+	it("calls copySectionsFromTo when the form is submitted", () => {
+		const fetchAuditCycles = jest.fn();
+		const preventDefault = jest.fn();
+		copySectionsFromTo.mockResolvedValue([]);
+
+		const r = shallow(<__SectionCopyForm auditCycle={sampleAuditCycle} otherAuditCycles={sampleAuditCycles} fetchAuditCycles={fetchAuditCycles}/>);
+
+		r.find("FormSelect").simulate("change", { target: { name: "selectedAuditCycleId", value: "2"}});
+		r.find("form").simulate("submit", { preventDefault });
+
+		expect(copySectionsFromTo).toBeCalledWith("2", sampleAuditCycle.id);
+	});
+
+	it("calls form.preventDefault when the form is submitted", () => {
+		const fetchAuditCycles = jest.fn();
+		const preventDefault = jest.fn();
+		copySectionsFromTo.mockResolvedValue([]);
+
+		const r = shallow(<__SectionCopyForm auditCycle={sampleAuditCycle} otherAuditCycles={sampleAuditCycles} fetchAuditCycles={fetchAuditCycles}/>);
+
+		r.find("form").simulate("submit", { preventDefault });
+
+		expect(preventDefault).toBeCalled();
+	});
+});
