919e6ddf6e8778df9a18f1fbee07c80bd9db96f9
satyanash <satyajeet@webexports.co.in>
2018-08-05 09:36:43 +0530

    convert post approval description field to text area in auditcycle admin form

--- audit/admin.py
+++ audit/admin.py
@@ -1,11 +1,18 @@
 from django.contrib import admin
 from django.contrib.admin import ModelAdmin
+from django.forms import Textarea
 
 from .models import AuditCycle, Audit
 
 
 class AuditCycleAdmin(ModelAdmin):
     list_filter = ('client', 'type')
+
+    def formfield_for_dbfield(self, db_field, **kwargs):
+        formfield = super(AuditCycleAdmin, self).formfield_for_dbfield(db_field, **kwargs)
+        if db_field.name == 'post_approval_description':
+            formfield.widget = Textarea(attrs=formfield.widget.attrs)
+        return formfield
 
 
 class AuditAdmin(ModelAdmin):
