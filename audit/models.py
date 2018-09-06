c938098f884b2c85e267cd3b5c9a89fe818176df
Amit Gaiki <xamit.94@gmail.com>
2018-09-06 21:49:12 +0530

    increase character limit of description and post approval description for audit cycle

--- audit/models.py
+++ audit/models.py
@@ -75,9 +75,9 @@ class AuditCycle(Model):
     earnings_per_audit = IntegerField(db_column='earnings_per_audit', blank=True, null=True)
     revenue_per_audit = IntegerField(db_column='revenue_per_audit', blank=True, null=True)
     reimbursement = IntegerField(db_column='reimbursement', blank=True, null=True)
-    description = CharField(db_column='description', max_length=4096, blank=False)
+    description = CharField(db_column='description', max_length=16384, blank=False)
     client = ForeignKey('client.Client', related_name='audits', db_column='client_id', on_delete=PROTECT)
-    post_approval_description = CharField(db_column='post_approval_description', max_length=4096, blank=True)
+    post_approval_description = CharField(db_column='post_approval_description', max_length=16384, blank=True)
     questionnaire_type = ForeignKey('questionnaire.QuestionnaireType', db_column='questionnaire_type_id', null=True, on_delete=PROTECT)
 
     class Meta:
