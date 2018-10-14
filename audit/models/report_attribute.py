e1b7a7e0faa2cdab26e1fe0d841243cb0d7491d3
Amit Gaiki <xamit.94@gmail.com>
2018-10-14 14:14:00 +0530

    +Satyajeet | add clean method to validate attribute data for a ReportAttribute

--- audit/models/report_attribute.py
+++ audit/models/report_attribute.py
@@ -2,11 +2,48 @@ from django.db.models import Model, CharField, IntegerField, AutoField, DateFiel
 from django.db.models import PROTECT, F, Sum
 from django.contrib.postgres.fields import JSONField
 
+from jsonschema import validate
+from jsonschema.exceptions import ValidationError
+
+from kronos.exceptions import AppLogicError
+
 
 class ReportAttribute(Model):
+    ATTRIBUTE_DATA_SCHEMA_V1 = {
+        "type": "object",
+        "required": ["version", "options"],
+        "properties": {
+            "version": {
+                "type": "integer",
+            },
+            "options": {
+                "type": "array",
+                "uniqueItems": True,
+                "minItems": 2,
+                "items": {
+                    "type": "object",
+                    "required": ["option_id", "option_label"],
+                    "properties": {
+                        "option_id": {
+                            "type": "string",
+                        },
+                        "option_label": {
+                            "type": "string",
+                        },
+                    },
+                }
+            },
+        },
+    }
+
     id = AutoField(db_column='id', primary_key=True)
     json_id = CharField(db_column='json_id', max_length=20, blank=False)
     label = CharField(db_column='label', max_length=50, blank=False)
     audit_cycle = ForeignKey('audit.AuditCycle', db_column='audit_cycle_id', related_name='report_attributes', on_delete=PROTECT)
     attribute_data = JSONField(db_column='attribute_data', default=dict(), blank=False)
 
+    def clean(self):
+        try:
+            validate(self.attribute_data, self.ATTRIBUTE_DATA_SCHEMA_V1)
+        except ValidationError as v:
+            raise AppLogicError(v.message) from v
---
+++ audit/models/tests/test_report_attribute.py
@@ -0,0 +1,105 @@
+from django.test import TestCase
+
+from model_mommy import mommy
+
+from kronos.exceptions import AppLogicError
+from audit.models import ReportAttribute, AuditCycle
+
+
+class ReportAttributeTestCase(TestCase):
+
+    def test_clean_does_not_raise_for_valid_attribute_data(self):
+        report_attribute = ReportAttribute()
+        valid_attribute_data = {
+            "version": 1,
+            "options": [
+                {
+                    "option_id": "1",
+                    "option_label": "label1"
+                },
+                {
+                    "option_id": "2",
+                    "option_label": "label2"
+                },
+            ],
+        }
+        report_attribute.attribute_data = valid_attribute_data
+        report_attribute.clean()
+
+    def test_clean_raises_when_version_is_missing(self):
+        report_attribute = ReportAttribute()
+        valid_attribute_data = {
+            "options": [
+                {
+                    "option_id": "1",
+                    "option_label": "label1"
+                },
+                {
+                    "option_id": "2",
+                    "option_label": "label2"
+                },
+            ],
+        }
+        report_attribute.attribute_data = valid_attribute_data
+        with self.assertRaisesRegex(AppLogicError, "'version' is a required property"):
+            report_attribute.clean()
+
+    def test_clean_raises_when_options_key_is_missing(self):
+        report_attribute = ReportAttribute()
+        valid_attribute_data = {
+            "version": 1
+        }
+        report_attribute.attribute_data = valid_attribute_data
+        with self.assertRaisesRegex(AppLogicError, "'options' is a required property"):
+            report_attribute.clean()
+
+    def test_clean_raises_when_options_length_is_less_than_two(self):
+        report_attribute = ReportAttribute()
+        valid_attribute_data = {
+            "version": 1,
+            "options": [
+                {
+                    "option_id": "1",
+                    "option_label": "label1"
+                },
+            ],
+        }
+        report_attribute.attribute_data = valid_attribute_data
+        with self.assertRaisesRegex(AppLogicError, ".* is too short"):
+            report_attribute.clean()
+
+    def test_clean_raises_when_options_contains_duplicates(self):
+        report_attribute = ReportAttribute()
+        valid_attribute_data = {
+            "version": 1,
+            "options": [
+                {
+                    "option_id": "1",
+                    "option_label": "label1"
+                },
+                {
+                    "option_id": "1",
+                    "option_label": "label1"
+                },
+            ],
+        }
+        report_attribute.attribute_data = valid_attribute_data
+        with self.assertRaisesRegex(AppLogicError, ".* has non-unique element"):
+            report_attribute.clean()
+
+    def test_clean_raises_when_option_id_are_not_present(self):
+        report_attribute = ReportAttribute()
+        valid_attribute_data = {
+            "version": 1,
+            "options": [
+                {
+                    "option_label": "label1"
+                },
+                {
+                    "option_label": "label2"
+                },
+            ],
+        }
+        report_attribute.attribute_data = valid_attribute_data
+        with self.assertRaisesRegex(AppLogicError, "'option_id' is a required property"):
+            report_attribute.clean()
