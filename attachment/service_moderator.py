726dfe0d2d76f04b9530b5cd432a7b07e84f6c12
satyanash <satyanash@dinosaurs.sexy>
2018-12-02 22:41:30 +0530

    add moderator wrapper to upload attachments for an answer

--- attachment/service_moderator.py
+++ attachment/service_moderator.py
@@ -3,6 +3,7 @@ from kronos.exceptions import AppLogicError
 
 import audit_store.service_moderator as audit_store_service
 import answer.service.report_section_moderator as report_section_moderator_service
+import answer.service.answer_moderator as answer_moderator_service
 
 from . import service as attachment_service
 
@@ -33,6 +34,14 @@ def upload_for_report_section_for_moderator(audit_store_id, section_id, file_nam
         raise AppLogicError("cannot upload attachment now")
 
     return attachment_service.upload_for_report_section(audit_store_id, section_id, file_name, file_size, mime_type)
+
+def upload_for_answer_for_moderator(audit_store_id, question_id, file_name, file_size, mime_type, user_id):
+    answer = answer_moderator_service.find_by_audit_store_and_question_for_moderator(audit_store_id, question_id, user_id)
+
+    if not answer.audit_store.is_editable_by_moderator():
+        raise AppLogicError("cannot upload attachment now")
+
+    return attachment_service.upload_for_answer(audit_store_id, answer.question_id, file_name, file_size, mime_type)
 
 def rename_for_moderator(attachment_id, user_id, new_name):
     audit_store = attachment_service.get_audit_store_for_attachment(attachment_id)
--- attachment/tests/test_service_moderator.py
+++ attachment/tests/test_service_moderator.py
@@ -2,6 +2,7 @@ from django.test import TestCase, override_settings
 from django.contrib.auth.models import User, Group
 from django.contrib.contenttypes.models import ContentType
 
+from expects import expect, be_a, be_an
 from model_mommy import mommy
 from model_mommy.recipe import Recipe
 
@@ -9,27 +10,29 @@ from guardian.shortcuts import assign_perm
 
 from kronos.exceptions import AppLogicError
 from attachment.models import Attachment
-from questionnaire.models import Section
+from questionnaire.models import Section, Question
+from answer.models import ReportSection, Answer
 from attachment import service_moderator as attachment_moderator_service
 from audit_store.models import AuditStore
 from audit.models import AuditCycle
 from registration.models import GROUP_NAME_MODERATOR, GROUP_NAME_AUDITOR
 from auditor.models import ProfileInfo
 
+test_aws_settings = {
+    "S3_ATTACHMENTS": {
+        "AWS_ACCESS_KEY_ID": "foo",
+        "AWS_SECRET_ACCESS_KEY": "bar",
+        "BUCKET": "floorwalk-attachments",
+        "REGION": "ap-southeast-1",
+        "MIN_SIZE": 10,
+        "MAX_SIZE": 1024 * 1024,
+        "FILE_SLUG_SIZE": 100,
+    }
+}
+
+@override_settings(AWS = test_aws_settings)
 class AttachmentModeratorServiceTestCase(TestCase):
     fixtures = ['groups']
-
-    test_aws_settings = {
-        "S3_ATTACHMENTS": {
-            "AWS_ACCESS_KEY_ID": "foo",
-            "AWS_SECRET_ACCESS_KEY": "bar",
-            "BUCKET": "floorwalk-attachments",
-            "REGION": "ap-southeast-1",
-            "MIN_SIZE": 10,
-            "MAX_SIZE": 1024 * 1024,
-            "FILE_SLUG_SIZE": 100,
-        }
-    }
 
     def setUp(self):
         self.auditor_group = Group.objects.get(name=GROUP_NAME_AUDITOR)
@@ -40,16 +43,16 @@ class AttachmentModeratorServiceTestCase(TestCase):
                                        groups=[self.auditor_group])
         self.auditor_profile = mommy.make(ProfileInfo, user=self.auditor_user)
 
-    @override_settings(AWS = test_aws_settings)
     def test_upload_for_audit_store_for_moderator_creates_attachment_based_on_status(self):
         audit_store_recipe = Recipe(AuditStore, user=self.auditor_user)
+        test_file = "hello_world.jpg"
+        test_mime_type = "image/jpeg"
+        test_size = 2048
+
         for status in [s[0] for s in AuditStore.STATUS if s[0] is not AuditStore.WITHDRAWN]:
             audit_cycle = mommy.make(AuditCycle, status=AuditCycle.ACTIVE)
             audit_store = audit_store_recipe.make(status=status, audit__audit_cycle=audit_cycle)
             assign_perm('audit_store.moderator_manage', self.moderator_user, audit_store)
-            test_file = "hello_world.jpg"
-            test_mime_type = "image/jpeg"
-            test_size = 2048
             if audit_store.status in audit_store._MODERATOR_EDITABLE_STATUSES:
                 post_data, created_attachment = attachment_moderator_service.upload_for_audit_store_for_moderator(
                     audit_store.id,
@@ -73,17 +76,17 @@ class AttachmentModeratorServiceTestCase(TestCase):
                         test_mime_type,
                     )
 
-    @override_settings(AWS = test_aws_settings)
     def test_upload_for_report_section_for_moderator_creates_attachment_based_on_audit_store_status(self):
         audit_store_recipe = Recipe(AuditStore, user=self.auditor_user)
+        test_file = "hello_world.jpg"
+        test_mime_type = "image/jpeg"
+        test_size = 2048
+
         for status in [s[0] for s in AuditStore.STATUS if s[0] is not AuditStore.WITHDRAWN]:
             audit_cycle = mommy.make(AuditCycle, status=AuditCycle.ACTIVE)
             section = mommy.make(Section, audit_cycle=audit_cycle)
             audit_store = audit_store_recipe.make(status=status, audit__audit_cycle=audit_cycle)
             assign_perm('audit_store.moderator_manage', self.moderator_user, audit_store)
-            test_file = "hello_world.jpg"
-            test_mime_type = "image/jpeg"
-            test_size = 2048
             if audit_store.status in audit_store._MODERATOR_EDITABLE_STATUSES:
                 post_data, created_attachment = attachment_moderator_service.upload_for_report_section_for_moderator(
                     audit_store.id,
@@ -98,6 +101,7 @@ class AttachmentModeratorServiceTestCase(TestCase):
                 self.assertEqual(test_mime_type, created_attachment.mime_type)
                 self.assertEqual(Attachment.PHOTO, created_attachment.proof_type)
                 self.assertEqual(Attachment.UPLOADING, created_attachment.status)
+                expect(created_attachment.content_object).to(be_a(ReportSection))
             else:
                 with self.assertRaisesRegex(AppLogicError, "cannot upload attachment now"):
                     attachment_moderator_service.upload_for_report_section_for_moderator(
@@ -109,7 +113,43 @@ class AttachmentModeratorServiceTestCase(TestCase):
                         self.moderator_user.id,
                     )
 
-    @override_settings(AWS = test_aws_settings)
+    def test_upload_for_answer_for_moderator_creates_attachment_based_on_audit_store_status(self):
+        audit_store_recipe = Recipe(AuditStore, user=self.auditor_user)
+        test_file = "hello_world.jpg"
+        test_mime_type = "image/jpeg"
+        test_size = 2048
+
+        for status in [s[0] for s in AuditStore.STATUS if s[0] is not AuditStore.WITHDRAWN]:
+            audit_cycle = mommy.make(AuditCycle, status=AuditCycle.ACTIVE)
+            question = mommy.make(Question, section__audit_cycle=audit_cycle)
+            audit_store = audit_store_recipe.make(status=status, audit__audit_cycle=audit_cycle)
+            assign_perm('audit_store.moderator_manage', self.moderator_user, audit_store)
+            if audit_store.status in audit_store._MODERATOR_EDITABLE_STATUSES:
+                post_data, created_attachment = attachment_moderator_service.upload_for_answer_for_moderator(
+                    audit_store.id,
+                    question.id,
+                    test_file,
+                    test_size,
+                    test_mime_type,
+                    self.moderator_user.id,
+                )
+                self.assertEqual(test_file, created_attachment.file_name)
+                self.assertEqual(test_size, created_attachment.file_size)
+                self.assertEqual(test_mime_type, created_attachment.mime_type)
+                self.assertEqual(Attachment.PHOTO, created_attachment.proof_type)
+                self.assertEqual(Attachment.UPLOADING, created_attachment.status)
+                expect(created_attachment.content_object).to(be_an(Answer))
+            else:
+                with self.assertRaisesRegex(AppLogicError, "cannot upload attachment now"):
+                    attachment_moderator_service.upload_for_answer_for_moderator(
+                        audit_store.id,
+                        question.id,
+                        test_file,
+                        test_size,
+                        test_mime_type,
+                        self.moderator_user.id,
+                    )
+
     def test_complete_for_moderator_completes_attachment_based_on_audit_store_status(self):
         audit_store_recipe = Recipe(AuditStore, user=self.auditor_user)
         for status in [s[0] for s in AuditStore.STATUS if s[0] is not AuditStore.WITHDRAWN]:
@@ -130,7 +170,6 @@ class AttachmentModeratorServiceTestCase(TestCase):
                 with self.assertRaisesRegex(AppLogicError, "cannot complete attachment now"):
                     attachment_moderator_service.complete_for_moderator(attachment.id, self.moderator_user.id)
 
-    @override_settings(AWS = test_aws_settings)
     def test_delete_for_moderator_deletes_attachment_based_on_audit_store_status(self):
         audit_store_recipe = Recipe(AuditStore, user=self.auditor_user)
         for status in [s[0] for s in AuditStore.STATUS if s[0] is not AuditStore.WITHDRAWN]:
@@ -150,7 +189,6 @@ class AttachmentModeratorServiceTestCase(TestCase):
                 with self.assertRaisesRegex(AppLogicError, "cannot delete attachment now"):
                     attachment_moderator_service.delete_for_moderator(attachment.id, self.moderator_user.id)
 
-    @override_settings(AWS = test_aws_settings)
     def test_rename_for_moderator_renames_attachment_based_on_audit_store_status(self):
         audit_store_recipe = Recipe(AuditStore, user=self.auditor_user)
         for status in [s[0] for s in AuditStore.STATUS if s[0] is not AuditStore.WITHDRAWN]:
