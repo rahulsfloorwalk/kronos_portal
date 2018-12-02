53bc685184c88668bae11843e406b1d01a940d72
satyanash <satyanash@dinosaurs.sexy>
2018-12-02 22:24:44 +0530

    add agency wrapper to upload attachment for an answer

--- attachment/service_agency.py
+++ attachment/service_agency.py
@@ -1,6 +1,7 @@
 from kronos.exceptions import AppLogicError, ObjectNotFound
 from audit_store import service_agency as audit_store_service
 from answer.service import report_section_agency as report_section_agency_service
+from answer.service import answer_agency as answer_agency_service
 from attachment import service as attachment_service
 from audit_store.models import AuditStore
 from answer.models import ReportSection
@@ -10,16 +11,17 @@ def upload_for_audit_store_by_agency(audit_store_id, file_name, file_size, mime_
     audit_store = audit_store_service.find_by_id_for_agency_user(audit_store_id, user_id)
     return attachment_service.upload_for_audit_store(audit_store.id, file_name, file_size, mime_type)
 
-
 def upload_for_report_section_by_agency(audit_store_id, section_id, file_name, file_size, mime_type, user_id):
     report_section = report_section_agency_service.find_by_audit_store_and_section_for_agency(audit_store_id, section_id, user_id)
     return attachment_service.upload_for_report_section(audit_store_id, report_section.section_id, file_name, file_size, mime_type)
 
+def upload_for_answer_by_agency(audit_store_id, question_id, file_name, file_size, mime_type, user_id):
+    answer = answer_agency_service.find_by_audit_store_and_question_for_agency(audit_store_id, question_id, user_id)
+    return attachment_service.upload_for_answer(audit_store_id, answer.question_id, file_name, file_size, mime_type)
 
 def find_by_audit_store_for_agency(audit_store_id, user_id):
     audit_store = audit_store_service.find_by_id_for_agency_user(audit_store_id, user_id)
     return attachment_service.find_by_audit_store(audit_store.id)
-
 
 def find_by_audit_store_and_section_for_agency(audit_store_id, section_id, user_id):
     report_section = report_section_agency_service.find_by_audit_store_and_section_for_agency(audit_store_id, section_id, user_id)
--- attachment/tests/test_service_agency.py
+++ attachment/tests/test_service_agency.py
@@ -2,33 +2,38 @@ from django.test import TestCase, override_settings
 from django.contrib.auth.models import User, Group
 from django.contrib.contenttypes.models import ContentType
 
+from expects import expect, equal, be_a, be_an
 from model_mommy import mommy
 
 from kronos.exceptions import AppLogicError, ObjectNotFound
 from attachment.models import Attachment
-from questionnaire.models import Section
-from answer.models import ReportSection
+from questionnaire.models import Section, Question
+from answer.models import ReportSection, Answer
 from attachment import service as attachment_service
 from attachment import service_agency as attachment_agency_service
 from audit_store.models import AuditStore
 from audit.models import AuditCycle
 from registration.models import GROUP_NAME_AGENCY
 
+test_aws_settings = {
+    "S3_ATTACHMENTS": {
+        "AWS_ACCESS_KEY_ID": "foo",
+        "AWS_SECRET_ACCESS_KEY": "bar",
+        "BUCKET": "floorwalk-attachments",
+        "REGION": "ap-southeast-1",
+        "MIN_SIZE": 10,
+        "MAX_SIZE": 1024 * 1024,
+        "FILE_SLUG_SIZE": 100,
+    }
+}
 
+@override_settings(AWS = test_aws_settings)
 class AttachmentAgencyServiceTestCase(TestCase):
     fixtures = ['groups']
 
-    test_aws_settings = {
-        "S3_ATTACHMENTS": {
-            "AWS_ACCESS_KEY_ID": "foo",
-            "AWS_SECRET_ACCESS_KEY": "bar",
-            "BUCKET": "floorwalk-attachments",
-            "REGION": "ap-southeast-1",
-            "MIN_SIZE": 10,
-            "MAX_SIZE": 1024 * 1024,
-            "FILE_SLUG_SIZE": 100,
-        }
-    }
+    test_file = "hello_world.jpg"
+    test_mime_type = "image/jpeg"
+    test_size = 2048
 
     def setUp(self):
         self.agency_group = Group.objects.get(name=GROUP_NAME_AGENCY)
@@ -37,63 +42,73 @@ class AttachmentAgencyServiceTestCase(TestCase):
         self.another_agency_user = mommy.make(User, username="another@foobar.com", email="another@foobar.com",
                                               groups=[self.agency_group])
 
-    @override_settings(AWS = test_aws_settings)
     def test_upload_for_audit_store_for_agency_creates_attachment(self):
         audit_cycle = mommy.make(AuditCycle, status=AuditCycle.ACTIVE)
         audit_store = mommy.make(AuditStore, status=AuditStore.ACKNOWLEDGED, user=self.agency_user, audit__audit_cycle=audit_cycle)
-        test_file = "hello_world.jpg"
-        test_mime_type = "image/jpeg"
-        test_size = 2048
         post_data, created_attachment = attachment_agency_service.upload_for_audit_store_by_agency(
             audit_store.id,
-            test_file,
-            test_size,
-            test_mime_type,
+            self.test_file,
+            self.test_size,
+            self.test_mime_type,
             self.agency_user.id,
         )
-        self.assertEqual(test_file, created_attachment.file_name)
-        self.assertEqual(test_size, created_attachment.file_size)
-        self.assertEqual(test_mime_type, created_attachment.mime_type)
-        self.assertEqual(Attachment.PHOTO, created_attachment.proof_type)
-        self.assertEqual(Attachment.UPLOADING, created_attachment.status)
+        expect(created_attachment.file_name).to(equal(self.test_file))
+        expect(created_attachment.file_size).to(equal(self.test_size))
+        expect(created_attachment.mime_type).to(equal(self.test_mime_type))
+        expect(created_attachment.proof_type).to(equal(Attachment.PHOTO))
+        expect(created_attachment.status).to(equal(Attachment.UPLOADING))
+        expect(created_attachment.content_object).to(be_an(AuditStore))
 
-    @override_settings(AWS=test_aws_settings)
     def test_upload_for_report_section_for_agency_creates_attachment(self):
         audit_cycle = mommy.make(AuditCycle, status=AuditCycle.ACTIVE)
         audit_store = mommy.make(AuditStore, status=AuditStore.ACKNOWLEDGED, user=self.agency_user,
                                  audit__audit_cycle=audit_cycle)
         section = mommy.make(Section, audit_cycle=audit_cycle)
-        test_file = "hello_world.jpg"
-        test_mime_type = "image/jpeg"
-        test_size = 2048
         post_data, created_attachment = attachment_agency_service.upload_for_report_section_by_agency(
             audit_store.id,
             section.id,
-            test_file,
-            test_size,
-            test_mime_type,
+            self.test_file,
+            self.test_size,
+            self.test_mime_type,
             self.agency_user.id,
         )
-        self.assertEqual(test_file, created_attachment.file_name)
-        self.assertEqual(test_size, created_attachment.file_size)
-        self.assertEqual(test_mime_type, created_attachment.mime_type)
-        self.assertEqual(Attachment.PHOTO, created_attachment.proof_type)
-        self.assertEqual(Attachment.UPLOADING, created_attachment.status)
+        expect(created_attachment.file_name).to(equal(self.test_file))
+        expect(created_attachment.file_size).to(equal(self.test_size))
+        expect(created_attachment.mime_type).to(equal(self.test_mime_type))
+        expect(created_attachment.proof_type).to(equal(Attachment.PHOTO))
+        expect(created_attachment.status).to(equal(Attachment.UPLOADING))
+        expect(created_attachment.content_object).to(be_a(ReportSection))
 
-    @override_settings(AWS=test_aws_settings)
+    def test_upload_for_answer_for_agency_creates_attachment(self):
+        audit_cycle = mommy.make(AuditCycle, status=AuditCycle.ACTIVE)
+        audit_store = mommy.make(AuditStore, status=AuditStore.ACKNOWLEDGED, user=self.agency_user,
+                                 audit__audit_cycle=audit_cycle)
+        question = mommy.make(Question, section__audit_cycle=audit_cycle)
+        post_data, created_attachment = attachment_agency_service.upload_for_answer_by_agency(
+            audit_store.id,
+            question.id,
+            self.test_file,
+            self.test_size,
+            self.test_mime_type,
+            self.agency_user.id,
+        )
+        expect(created_attachment.file_name).to(equal(self.test_file))
+        expect(created_attachment.file_size).to(equal(self.test_size))
+        expect(created_attachment.mime_type).to(equal(self.test_mime_type))
+        expect(created_attachment.proof_type).to(equal(Attachment.PHOTO))
+        expect(created_attachment.status).to(equal(Attachment.UPLOADING))
+        expect(created_attachment.content_object).to(be_an(Answer))
+
     def test_find_by_audit_store_for_agency_returns_attachment(self):
         audit_cycle = mommy.make(AuditCycle, status=AuditCycle.ACTIVE)
         audit_store = mommy.make(AuditStore, status=AuditStore.ACKNOWLEDGED, user=self.agency_user,
                                  audit__audit_cycle=audit_cycle)
-        test_file = "hello_world.jpg"
-        test_mime_type = "image/jpeg"
-        test_size = 2048
         mommy.make(
             Attachment,
             status=Attachment.ATTACHED,
-            file_name=test_file,
-            file_size=test_size,
-            mime_type=test_mime_type,
+            file_name=self.test_file,
+            file_size=self.test_size,
+            mime_type=self.test_mime_type,
             content_type=ContentType.objects.get_for_model(AuditStore),
             object_id=audit_store.id,
         )
@@ -102,27 +117,23 @@ class AttachmentAgencyServiceTestCase(TestCase):
             self.agency_user.id,
         )
         self.assertEqual(1, len(attachments))
-        self.assertEqual(test_file, attachments[0].file_name)
-        self.assertEqual(test_size, attachments[0].file_size)
-        self.assertEqual(test_mime_type, attachments[0].mime_type)
+        self.assertEqual(self.test_file, attachments[0].file_name)
+        self.assertEqual(self.test_size, attachments[0].file_size)
+        self.assertEqual(self.test_mime_type, attachments[0].mime_type)
         self.assertEqual(Attachment.ATTACHED, attachments[0].status)
 
-    @override_settings(AWS=test_aws_settings)
     def test_find_by_audit_store_and_section_for_agency_returns_attachment(self):
         audit_cycle = mommy.make(AuditCycle, status=AuditCycle.ACTIVE)
         audit_store = mommy.make(AuditStore, status=AuditStore.ACKNOWLEDGED, user=self.agency_user,
                                  audit__audit_cycle=audit_cycle)
         section = mommy.make(Section, audit_cycle=audit_cycle)
         report_section = mommy.make(ReportSection, section=section, audit_store=audit_store)
-        test_file = "hello_world.jpg"
-        test_mime_type = "image/jpeg"
-        test_size = 2048
         mommy.make(
             Attachment,
             status=Attachment.ATTACHED,
-            file_name=test_file,
-            file_size=test_size,
-            mime_type=test_mime_type,
+            file_name=self.test_file,
+            file_size=self.test_size,
+            mime_type=self.test_mime_type,
             content_type=ContentType.objects.get_for_model(ReportSection),
             object_id=report_section.id,
         )
@@ -132,27 +143,23 @@ class AttachmentAgencyServiceTestCase(TestCase):
             self.agency_user.id,
         )
         self.assertEqual(1, len(attachments))
-        self.assertEqual(test_file, attachments[0].file_name)
-        self.assertEqual(test_size, attachments[0].file_size)
-        self.assertEqual(test_mime_type, attachments[0].mime_type)
+        self.assertEqual(self.test_file, attachments[0].file_name)
+        self.assertEqual(self.test_size, attachments[0].file_size)
+        self.assertEqual(self.test_mime_type, attachments[0].mime_type)
         self.assertEqual(Attachment.ATTACHED, attachments[0].status)
 
-    @override_settings(AWS=test_aws_settings)
     def test_complete_for_agency_changes_status_as_attached(self):
         audit_cycle = mommy.make(AuditCycle, status=AuditCycle.ACTIVE)
         audit_store = mommy.make(AuditStore, status=AuditStore.ACKNOWLEDGED, user=self.agency_user,
                                  audit__audit_cycle=audit_cycle)
         section = mommy.make(Section, audit_cycle=audit_cycle)
         report_section = mommy.make(ReportSection, section=section, audit_store=audit_store)
-        test_file = "hello_world.jpg"
-        test_mime_type = "image/jpeg"
-        test_size = 2048
         attachment = mommy.make(
             Attachment,
             status=Attachment.UPLOADING,
-            file_name=test_file,
-            file_size=test_size,
-            mime_type=test_mime_type,
+            file_name=self.test_file,
+            file_size=self.test_size,
+            mime_type=self.test_mime_type,
             content_type=ContentType.objects.get_for_model(ReportSection),
             object_id=report_section.id,
         )
@@ -160,27 +167,23 @@ class AttachmentAgencyServiceTestCase(TestCase):
             attachment.id,
             self.agency_user.id,
         )
-        self.assertEqual(test_file, completed_attachment.file_name)
-        self.assertEqual(test_size, completed_attachment.file_size)
-        self.assertEqual(test_mime_type, completed_attachment.mime_type)
+        self.assertEqual(self.test_file, completed_attachment.file_name)
+        self.assertEqual(self.test_size, completed_attachment.file_size)
+        self.assertEqual(self.test_mime_type, completed_attachment.mime_type)
         self.assertEqual(Attachment.ATTACHED, completed_attachment.status)
 
-    @override_settings(AWS=test_aws_settings)
     def test_complete_for_agency_raises_exception_when_user_is_different(self):
         audit_cycle = mommy.make(AuditCycle, status=AuditCycle.ACTIVE)
         audit_store = mommy.make(AuditStore, status=AuditStore.ASSIGNED, user=self.agency_user,
                                  audit__audit_cycle=audit_cycle)
         section = mommy.make(Section, audit_cycle=audit_cycle)
         report_section = mommy.make(ReportSection, section=section, audit_store=audit_store)
-        test_file = "hello_world.jpg"
-        test_mime_type = "image/jpeg"
-        test_size = 2048
         attachment = mommy.make(
             Attachment,
             status=Attachment.UPLOADING,
-            file_name=test_file,
-            file_size=test_size,
-            mime_type=test_mime_type,
+            file_name=self.test_file,
+            file_size=self.test_size,
+            mime_type=self.test_mime_type,
             content_type=ContentType.objects.get_for_model(ReportSection),
             object_id=report_section.id,
         )
@@ -190,22 +193,18 @@ class AttachmentAgencyServiceTestCase(TestCase):
                 self.another_agency_user.id,
             )
 
-    @override_settings(AWS=test_aws_settings)
     def test_complete_for_agency_raises_exception_when_contenttype_is_different(self):
         audit_cycle = mommy.make(AuditCycle, status=AuditCycle.ACTIVE)
         audit_store = mommy.make(AuditStore, status=AuditStore.ASSIGNED, user=self.agency_user,
                                  audit__audit_cycle=audit_cycle)
         section = mommy.make(Section, audit_cycle=audit_cycle)
         report_section = mommy.make(ReportSection, section=section, audit_store=audit_store)
-        test_file = "hello_world.jpg"
-        test_mime_type = "image/jpeg"
-        test_size = 2048
         attachment = mommy.make(
             Attachment,
             status=Attachment.UPLOADING,
-            file_name=test_file,
-            file_size=test_size,
-            mime_type=test_mime_type,
+            file_name=self.test_file,
+            file_size=self.test_size,
+            mime_type=self.test_mime_type,
             content_type=ContentType.objects.get_for_model(Section),
             object_id=report_section.id,
         )
@@ -215,22 +214,18 @@ class AttachmentAgencyServiceTestCase(TestCase):
                 self.another_agency_user.id,
             )
 
-    @override_settings(AWS=test_aws_settings)
     def test_complete_for_agency_raises_exception_when_audit_store_not_acknowledged(self):
         audit_cycle = mommy.make(AuditCycle, status=AuditCycle.ACTIVE)
         audit_store = mommy.make(AuditStore, status=AuditStore.ASSIGNED, user=self.agency_user,
                                  audit__audit_cycle=audit_cycle)
         section = mommy.make(Section, audit_cycle=audit_cycle)
         report_section = mommy.make(ReportSection, section=section, audit_store=audit_store)
-        test_file = "hello_world.jpg"
-        test_mime_type = "image/jpeg"
-        test_size = 2048
         attachment = mommy.make(
             Attachment,
             status=Attachment.UPLOADING,
-            file_name=test_file,
-            file_size=test_size,
-            mime_type=test_mime_type,
+            file_name=self.test_file,
+            file_size=self.test_size,
+            mime_type=self.test_mime_type,
             content_type=ContentType.objects.get_for_model(ReportSection),
             object_id=report_section.id,
         )
@@ -240,22 +235,18 @@ class AttachmentAgencyServiceTestCase(TestCase):
                 self.agency_user.id,
             )
 
-    @override_settings(AWS=test_aws_settings)
     def test_delete_for_agency_changes_status_as_deleted(self):
         audit_cycle = mommy.make(AuditCycle, status=AuditCycle.ACTIVE)
         audit_store = mommy.make(AuditStore, status=AuditStore.ACKNOWLEDGED, user=self.agency_user,
                                  audit__audit_cycle=audit_cycle)
         section = mommy.make(Section, audit_cycle=audit_cycle)
         report_section = mommy.make(ReportSection, section=section, audit_store=audit_store)
-        test_file = "hello_world.jpg"
-        test_mime_type = "image/jpeg"
-        test_size = 2048
         attachment = mommy.make(
             Attachment,
             status=Attachment.ATTACHED,
-            file_name=test_file,
-            file_size=test_size,
-            mime_type=test_mime_type,
+            file_name=self.test_file,
+            file_size=self.test_size,
+            mime_type=self.test_mime_type,
             content_type=ContentType.objects.get_for_model(ReportSection),
             object_id=report_section.id,
         )
@@ -266,22 +257,18 @@ class AttachmentAgencyServiceTestCase(TestCase):
         deleted_attachment = attachment_service.find_by_id(attachment.id)
         self.assertEqual(Attachment.DELETED, deleted_attachment.status)
 
-    @override_settings(AWS=test_aws_settings)
     def test_delete_for_agency_raises_exception(self):
         audit_cycle = mommy.make(AuditCycle, status=AuditCycle.ACTIVE)
         audit_store = mommy.make(AuditStore, status=AuditStore.ASSIGNED, user=self.agency_user,
                                  audit__audit_cycle=audit_cycle)
         section = mommy.make(Section, audit_cycle=audit_cycle)
         report_section = mommy.make(ReportSection, section=section, audit_store=audit_store)
-        test_file = "hello_world.jpg"
-        test_mime_type = "image/jpeg"
-        test_size = 2048
         attachment = mommy.make(
             Attachment,
             status=Attachment.UPLOADING,
-            file_name=test_file,
-            file_size=test_size,
-            mime_type=test_mime_type,
+            file_name=self.test_file,
+            file_size=self.test_size,
+            mime_type=self.test_mime_type,
             content_type=ContentType.objects.get_for_model(ReportSection),
             object_id=report_section.id,
         )
