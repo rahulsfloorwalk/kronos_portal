eead751ca49ecc69dc5d1ac5c30fc5559fb8d797
Amit Gaiki <xamit.94@gmail.com>
2018-12-06 08:57:43 +0530

    add message field to audit store handlers and signals

--- audit_store/handlers.py
+++ audit_store/handlers.py
@@ -10,9 +10,11 @@ def status_change_report_log_callback(sender, **kwargs):
     user_actor = kwargs['user_actor']
     status = kwargs['status']
     audit_store = kwargs['audit_store']
+    message = kwargs['message']
     report_status_log = ReportStatusLog()
     report_status_log.user_actor = user_actor
     report_status_log.status = status
+    report_status_log.message = message
     report_status_log.audit_store = audit_store
     report_status_log.created_at = timezone.now()
     report_status_log.save()
--- audit_store/signals.py
+++ audit_store/signals.py
@@ -1,2 +1,2 @@
 from django.dispatch import Signal
-audit_store_status_change = Signal(providing_args=["old_status", "status", "user_actor", "audit_store"])
+audit_store_status_change = Signal(providing_args=["old_status", "status", "user_actor", "message", "audit_store"])
