c99c69151cf8ecfcaff03c7db5e7ae37411217c4
Amit Gaiki <xamit.94@gmail.com>
2018-12-07 00:26:26 +0530

    increase page size to 800 for auditor search

--- manager/viewss/auditor.py
+++ manager/viewss/auditor.py
@@ -26,7 +26,7 @@ from referral.service import referral_auditor as referral_service
 
 class AuditorView(generics.ListAPIView):
     class AuditorViewPaginationClass(PageNumberPagination):
-        page_size = 200
+        page_size = 800
         page_size_query_param = 'page_size'
         max_page_size = 1000
 
