9cde4dcc17fb1d98deb1545c6c4c6acacc0aabb8
satyanash <satyanash@dinosaurs.sexy>
2018-12-14 21:45:58 +0530

    add selector to calculate questionnaire total for a given audit cycle

--- frontend/js/manager/selectors/__tests__/section.test.js
+++ frontend/js/manager/selectors/__tests__/section.test.js
@@ -1,32 +1,33 @@
-import { findSectionsByAuditCycleId } from "../section";
+import { findSectionsByAuditCycleId, findQuestionnaireTotalForAuditCycleId } from "../section";
+
+const sampleSections = {
+	4: {
+		id: 1,
+		sequence: 2,
+		name: "Hello World",
+		max_marks: 4,
+		audit_cycle: 4,
+	},
+	5: {
+		id: 2,
+		sequence: 1,
+		name: "Bye World",
+		max_marks: 3,
+		audit_cycle: 4,
+	},
+	6: {
+		id: 3,
+		sequence: 1,
+		name: "Slartibartfast",
+		max_marks: 2,
+		audit_cycle: 5,
+	},
+};
+const sampleStore = {
+	sections: sampleSections,
+};
 
 describe(findSectionsByAuditCycleId, () => {
-	const sampleSections = {
-		4: {
-			id: 1,
-			sequence: 2,
-			name: "Hello World",
-			max_marks: 4,
-			audit_cycle: 4,
-		},
-		5: {
-			id: 2,
-			sequence: 1,
-			name: "Bye World",
-			max_marks: 3,
-			audit_cycle: 4,
-		},
-		6: {
-			id: 3,
-			sequence: 1,
-			name: "Slartibartfast",
-			max_marks: 2,
-			audit_cycle: 5,
-		},
-	};
-	const sampleStore = {
-		sections: sampleSections,
-	};
 
 	it("should return the sections with given audit cycle id, sorted by sequence", () => {
 		expect(findSectionsByAuditCycleId(sampleStore, 4)).toEqual([sampleSections[5], sampleSections[4]]);
@@ -34,5 +35,15 @@ describe(findSectionsByAuditCycleId, () => {
 
 	it("should return an empty array when sections with given audit cycle id are not found", () => {
 		expect(findSectionsByAuditCycleId(sampleStore, 2)).toEqual([]);
+	});
+});
+
+describe(findQuestionnaireTotalForAuditCycleId, () => {
+	it("should summation of max_marks for sections with given audit cycle id ", () => {
+		expect(findQuestionnaireTotalForAuditCycleId(sampleStore, 4)).toEqual(7);
+	});
+
+	it("should return zero when there are no sections for given audit cycle id ", () => {
+		expect(findQuestionnaireTotalForAuditCycleId(sampleStore, 2)).toEqual(0);
 	});
 });
--- frontend/js/manager/selectors/section.js
+++ frontend/js/manager/selectors/section.js
@@ -4,3 +4,9 @@ export const findSectionsByAuditCycleId = (store, auditCycleId) => {
 		.filter(s => s.audit_cycle === auditCycleId)
 		.sort((s1, s2) => s1.sequence - s2.sequence);
 };
+
+export const findQuestionnaireTotalForAuditCycleId = (store, auditCycleId) => {
+	return Object.values(store.sections)
+		.filter(s => s.audit_cycle === auditCycleId)
+		.reduce((sum, section) => sum + section.max_marks, 0);
+};
