6c74b415895d0cfaba03f1a2750f60da55efe7d2
satyanash <satyanash@dinosaurs.sexy>
2019-02-02 01:20:26 +0530

    remove AuditDeSerializer from moderator serializers

--- moderator_rest/serializers.py
+++ moderator_rest/serializers.py
@@ -163,32 +163,6 @@ class AuditSerializerWithoutApplications(ModelSerializer):
         )
         read_only_fields = fields
 
-class AuditDeSerializer(ModelSerializer):
-    class Meta:
-        model = Audit
-        fields = (
-            'id',
-            'count',
-            'earnings_per_audit',
-            'reimbursement',
-            'store',
-            'audit_cycle',
-        )
-        read_only_fields = ('id',)
-        validators=[]
-
-    def deserialize(self):
-        if 'id' in self.context and self.context.get('id') is not None:
-            audit = Audit.objects.get(id=self.context.get('id'))
-        else:
-            audit = Audit()
-        audit.count = self.validated_data.get('count', audit.count)
-        audit.earnings_per_audit = self.validated_data.get('earnings_per_audit', audit.earnings_per_audit)
-        audit.reimbursement = self.validated_data.get('reimbursement', audit.reimbursement)
-        audit.store = self.validated_data.get('store', audit.store_id)
-        audit.audit_cycle = self.validated_data.get('audit_cycle', audit.audit_cycle_id)
-        return audit
-
 
 class AuditStoreSerializer(ModelSerializer):
     audit = AuditSerializerWithoutApplications()
