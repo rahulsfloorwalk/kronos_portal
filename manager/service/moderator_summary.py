1d96106ed2c22348c535ea26ebbded1a7ef553aa
satyanash <satyanash@dinosaurs.sexy>
2019-02-16 02:21:54 +0530

    optimize query for Moderator summary for an audit cycle

--- manager/service/moderator_summary.py
+++ manager/service/moderator_summary.py
@@ -1,15 +1,28 @@
+from django.contrib.contenttypes.models import ContentType
+from django.contrib.auth.models import Permission
+
+from guardian.models import UserObjectPermission
+
+from audit_store.models import AuditStore
 from audit.service import audit_cycle as audit_cycle_service
 from audit_store import service as audit_store_service
 
 def moderator_summary_for_audit_cycle(audit_cycle_id):
     audit_cycle = audit_cycle_service.find_by_id(audit_cycle_id)
-    reports = audit_store_service.find_by_audit_cycle(audit_cycle.id)
+    reports = audit_store_service.find_by_audit_cycle(audit_cycle.id).values("id", "status")
+    report_ids = [str(report["id"]) for report in reports]
+
+    content_type = ContentType.objects.get_for_model(AuditStore)
+    permission = Permission.objects.get(content_type=content_type, codename="moderator_manage")
+
+    perms = UserObjectPermission.objects.filter(content_type=content_type, object_pk__in=report_ids, permission=permission)
 
     data = {}
 
     for r in reports:
-        for moderator in r.assigned_to_moderator():
-            if moderator.id not in data:
-                data[moderator.id] = {}
-            data[moderator.id][r.status] = data[moderator.id].get(r.status, 0) + 1
+        for p in perms:
+            if str(r["id"]) == p.object_pk:
+                if p.user_id not in data:
+                    data[p.user_id] = {}
+                data[p.user_id][r["status"]] = data[p.user_id].get(r["status"], 0) + 1
     return data
