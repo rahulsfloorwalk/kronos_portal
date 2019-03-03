d94e742d3b1919aa0424ffb53be1bcf36917dde1
satyanash <satyanash@dinosaurs.sexy>
2019-03-03 19:07:01 +0530

    fix warnings in manager audit store details test after webpack 4 upgrade

--- frontend/js/manager/components/__tests__/AuditStoreDetails.test.jsx
+++ frontend/js/manager/components/__tests__/AuditStoreDetails.test.jsx
@@ -1,12 +1,15 @@
 import React from "react";
 import { Provider } from "react-redux";
 import { shallow } from "enzyme";
-import renderer from "react-test-renderer";
 import configureStore from "redux-mock-store";
 import ReduxThunk from "redux-thunk";
 
 import { AuditStoreDetails } from "../../../manager/components/AuditStoreDetails";
 import types from "../../../manager/action_types";
+
+import { fetchAuditStore, acceptAuditStore, pmRevertAuditStore, qaOkAuditStore } from "../../actions/audit_store";
+
+jest.mock("../../actions/audit_store");
 
 jest.mock("react-dom", () => ({
 	findDOMNode: () => {},
@@ -17,6 +20,12 @@ let dateNowSpy;
 beforeAll(() => {
 	// Lock Time
 	dateNowSpy = jest.spyOn(Date, "now").mockImplementation(() => 1527292800000);
+});
+
+afterAll(() => {
+	// Unlock Time
+	dateNowSpy.mockReset();
+	dateNowSpy.mockRestore();
 });
 
 const middlewares = [ReduxThunk];
@@ -89,25 +98,50 @@ describe("<AuditStoreDetails/>", () => {
 		};
 	});
 
+	beforeEach(() => {
+		fetchAuditStore.mockReturnValue({
+			type: types.AUDIT_STORE_ID_GET,
+			status: "request",
+			auditStoreId: sampleAuditStore.id,
+		});
+		qaOkAuditStore.mockReturnValue({
+			type: types.AUDIT_STORE_ID_QA_OK,
+			status: "request",
+			auditStoreId: sampleAuditStore.id,
+		});
+		pmRevertAuditStore.mockReturnValue({
+			type: types.AUDIT_STORE_ID_PM_REVERT,
+			status: "request",
+			auditStoreId: sampleAuditStore.id,
+		});
+		acceptAuditStore.mockReturnValue({
+			type: types.AUDIT_STORE_ID_ACCEPT,
+			status: "request",
+			auditStoreId: sampleAuditStore.id,
+		});
+	});
+
 	it("is rendered correctly when the AuditStore is loading", () => {
-		const r = renderer.create(<AuditStoreDetails auditStore={undefined} params={sampleParams} dispatch={store.dispatch} router={mockRouter}/>);
-		expect(r.toJSON()).toMatchSnapshot();
+		const r = shallow(<Provider store={sampleStore}>
+			<AuditStoreDetails auditStore={undefined} params={sampleParams} dispatch={store.dispatch} router={mockRouter}/>
+		</Provider>).dive();
+		expect(r).toMatchSnapshot();
 	});
 
 	it("is rendered correctly when AuditStore is not rated", () => {
-		const r = renderer.create(<Provider store={sampleStore}>
+		const r = shallow(<Provider store={sampleStore}>
 			<AuditStoreDetails auditStore={sampleAuditStore} params={sampleParams} dispatch={store.dispatch} router={mockRouter}/>
-		</Provider>);
+		</Provider>).dive();
 
-		expect(r.toJSON()).toMatchSnapshot();
+		expect(r).toMatchSnapshot();
 	});
 
 	it("is rendered correctly when AuditStore is already rated", () => {
 		const testAuditStore = Object.assign({}, sampleAuditStore, { qa_rating: 1});
-		const r = renderer.create(<Provider store={sampleStore}>
+		const r = shallow(<Provider store={sampleStore}>
 			<AuditStoreDetails auditStore={testAuditStore} params={sampleParams} dispatch={store.dispatch} router={mockRouter}/>
-		</Provider>);
-		expect(r.toJSON()).toMatchSnapshot();
+		</Provider>).dive();
+		expect(r).toMatchSnapshot();
 	});
 
 	const statuses = [
@@ -121,70 +155,66 @@ describe("<AuditStoreDetails/>", () => {
 		"WITHDRAWN",
 		"FAILED",
 	];
-	for( const s of statuses){
-		it(`is rendered correctly when status is ${s}`, () => {
-			const testAuditStore = Object.assign({}, sampleAuditStore, { status: s });
-			const r = renderer.create(<Provider store={sampleStore}>
-				<AuditStoreDetails auditStore={testAuditStore} params={sampleParams} dispatch={store.dispatch} router={mockRouter}/>
-			</Provider>);
-			expect(r.toJSON()).toMatchSnapshot();
-		});
-	}
+	test.each(statuses)("is rendered correctly when status is %s", (s) => {
+		const testAuditStore = Object.assign({}, sampleAuditStore, { status: s });
+		const r = shallow(<Provider store={sampleStore}>
+			<AuditStoreDetails auditStore={testAuditStore} params={sampleParams} dispatch={store.dispatch} router={mockRouter}/>
+		</Provider>).dive();
+		expect(r).toMatchSnapshot();
+	});
 
 	it("dispatches the qa ok request action when QA OK button is clicked", (done) => {
+		sampleStore.dispatch.mockResolvedValue("SUCCESS");
 		const testAuditStore = Object.assign({}, sampleAuditStore, { status: "SUBMITTED" });
-		const r = shallow(<AuditStoreDetails auditStore={testAuditStore} params={sampleParams} dispatch={store.dispatch} router={mockRouter}/>);
-		let qaOkButton = r.find("div.panel-footer > button").at(1);
+		const r = shallow(<AuditStoreDetails auditStore={testAuditStore} params={sampleParams} dispatch={sampleStore.dispatch} router={mockRouter}/>);
+		const qaOkButton = r.find("div.panel-footer > button").at(1);
 		expect(qaOkButton.length).toEqual(1);
 		expect(qaOkButton.text()).toEqual("Forward to PM");
 		qaOkButton.simulate("click");
 		setTimeout(() => {
-			expect(store.getActions()[1]).toEqual({
+			expect(sampleStore.dispatch).nthCalledWith(2, {
 				type: types.AUDIT_STORE_ID_QA_OK,
 				status: "request",
-				auditStoreId: sampleParams.auditStoreId,
+				auditStoreId: testAuditStore.id,
 			});
 			done();
 		});
 	});
 
 	it("dispatches the PM REVERT request action when PM REVERT button is clicked", (done) => {
+		sampleStore.dispatch.mockResolvedValue("SUCCESS");
 		const testAuditStore = Object.assign({}, sampleAuditStore, { status: "PM_REVIEW" });
-		const r = shallow(<AuditStoreDetails auditStore={testAuditStore} params={sampleParams} dispatch={store.dispatch} router={mockRouter}/>);
-		let qaOkButton = r.find("div.panel-footer > button").at(0);
+		const r = shallow(<AuditStoreDetails auditStore={testAuditStore} params={sampleParams} dispatch={sampleStore.dispatch} router={mockRouter}/>);
+		const qaOkButton = r.find("div.panel-footer > button").at(0);
 		expect(qaOkButton.length).toEqual(1);
 		expect(qaOkButton.text()).toEqual("Revert to QA");
 		qaOkButton.simulate("click");
 		setTimeout(() => {
-			expect(store.getActions()[1]).toEqual({
+			expect(sampleStore.dispatch).nthCalledWith(2, {
 				type: types.AUDIT_STORE_ID_PM_REVERT,
 				status: "request",
-				auditStoreId: sampleParams.auditStoreId,
+				auditStoreId: testAuditStore.id,
 			});
 			done();
 		});
 	});
 
 	it("dispatches the ACCEPT request action when ACCEPT button is clicked", (done) => {
+		sampleStore.dispatch.mockResolvedValue("SUCCESS");
 		const testAuditStore = Object.assign({}, sampleAuditStore, { status: "COMPLETED" });
-		const r = shallow(<AuditStoreDetails auditStore={testAuditStore} params={sampleParams} dispatch={store.dispatch} router={mockRouter}/>);
+		const r = shallow(<AuditStoreDetails auditStore={testAuditStore} params={sampleParams} dispatch={sampleStore.dispatch} router={mockRouter}/>);
+
 		const acceptButton = r.find("div.panel-footer > button").at(1);
 		expect(acceptButton.length).toEqual(1);
 		expect(acceptButton.text()).toEqual("Accept");
 		acceptButton.simulate("click");
 		setTimeout(() => {
-			expect(store.getActions()[1]).toEqual({
+			expect(sampleStore.dispatch).nthCalledWith(2, {
 				type: types.AUDIT_STORE_ID_ACCEPT,
 				status: "request",
-				auditStoreId: sampleParams.auditStoreId,
+				auditStoreId: testAuditStore.id,
 			});
 			done();
 		});
 	});
-});
-
-afterAll(() => {
-	// Unlock Time
-	dateNowSpy.mockReset();
-	dateNowSpy.mockRestore();
 });
