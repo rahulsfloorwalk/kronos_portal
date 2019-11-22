08588805b3b607c18844f68cece7e08f64f02790
ankush take <ankush.take@floorwalk.in>
2019-11-22 13:57:14 +0530

    code for all stores audit cycle wise excel

--- client_rest/views.py
+++ client_rest/views.py
@@ -26,6 +26,7 @@ from audit.service import report_attribute_client_service
 
 from client_report.service import ears_xlsx as ears_xlsx_report_service
 from client_report.service import xlsx_report as xlsx_report_service
+from client_report.service import all_stores_xlsx as all_stores_xlsx_report_service
 from client_report.service import audit_cycle_xlsx_report as cycle_xlsx_report_service
 from client_report.service import report_browser_xlsx as report_browser_xlsx_service
 from client_report.service import audit_section
@@ -249,6 +250,20 @@ class ReportBrowserFilteredXlsxReport(APIView):
         report, name = report_browser_xlsx_service.get_aggregate_report_with_filters(audit_cycle_id, request.user.id, filters)
         response = HttpResponse(report.read(), content_type='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet')
         response['Content-Disposition'] = 'attachment; filename="' + name + '"'
+        return response
+
+class AllStoresAuditCycleWiseXlsxReport(APIView):
+    permission_classes = [HasGroupPermission]
+    required_groups = {
+        'GET': [GROUP_NAME_CLIENT],
+    }
+    def get(self, request):
+        print("user_id",request.user.id)
+        report, name = all_stores_xlsx_report_service.generate_all_stores_audit_cycle_wise_report_for_clientuser(request.user.id)
+        # audit_cycles = audit_cycle_client_service.find_all_for_clientuser(request.user.id)
+        # return Response(audit_cycles)
+        response = HttpResponse(report.read(), content_type='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet')
+        response['Content-Disposition'] = 'attachment; filename="'+ name + '"'
         return response
 
 class AuditStoreEARSReport(APIView):
