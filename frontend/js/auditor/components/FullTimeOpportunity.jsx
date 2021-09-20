cf479d8d423fb5be0d3d8aa2efc63b2188874b69
Shubham Mohod <shubham.mohod@floorwalk.in>
2021-09-20 14:29:51 +0530

    full time opp url in auditor panel and fix reports

---
+++ frontend/js/auditor/components/FullTimeOpportunity.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+
+class FullTimeOpportunity extends React.Component{
+
+	render(){
+		return (
+			<div style={{fontSize:17,textAlign: "justify"}}>
+				<center>
+					<img src="https://floorwalk-client-logos.s3.ap-south-1.amazonaws.com/LOGOS/insurance-dekho.svg" style={{margin:"20px 0px",width:"40%"}}></img>
+				</center>
+				<p><b>BECOME AN INSURANCE AGENT - Earn up to 30% commission by selling insurance.</b></p>
+				<p>We are delighted to announce that FloorWalk has partnered with InsuranceDekho to enable earning opportunities for auditors PAN India in the insurance sector.</p>
+				<p>This partnership will generate new opportunities for FloorWalk auditors with an additional source of revenue. So, if you are looking to earn up to 30% commission by selling insurance across the country, you have landed on the right opportunity!</p>
+				<p>All you have to do is complete a simple registration process to become an agent.</p>
+				<p>Please note that by clicking on the Apply button, you agree to let FloorWalk share your details with Insurance Dekho for registration and training purposes and to be contacted by them at any point in time hereafter.</p><br/>
+				<p><b>Process Guidelines:</b></p>
+				<ol style={{listStyleType: "decimal"}}>
+					<li style={{padding: "5px 0px"}}>Submit your details by filling in the questionnaire provided, along with the necessary documents mentioned.</li>
+					<li style={{padding: "5px 0px"}}>Your lead will not be submitted to InsuranceDekho for further processes if your documents and details are incomplete or if they do not match to the information provided.</li>
+					<li style={{padding: "5px 0px"}}>Post submission of details, if your application is approved, you will be contacted by InsuranceDekho for further processes of induction and training.</li>
+					<li style={{padding: "5px 0px"}}>This is a sales-centric job, with an opportunity of earning up to 30% by selling insurance policies. Click on the following link to register yourself for the project - <a href="https://forms.gle/Rnd9J65iHkpGdtwr5" target="_blank" rel="noopener noreferrer">https://forms.gle/Rnd9J65iHkpGdtwr5</a></li>
+					<li style={{padding: "5px 0px"}}>Please note that your information will be shared with InsuranceDekho for Training and Communication Purposes. Only submit your information if you agree to the aforementioned criteria.</li>
+				</ol>
+				<br/><br/>
+				<p><b>APPLY BUTTON (LINK </b>- <a href="https://forms.gle/Rnd9J65iHkpGdtwr5" target="_blank" rel="noopener noreferrer">https://forms.gle/Rnd9J65iHkpGdtwr5</a><b>)</b></p>
+			</div>
+		);
+	}
+}
+
+export default FullTimeOpportunity;
