35dc56855ce37cf494df5f58a5e9da135506e3ab
arpan <arpan.patidar@floorwalk.in>
2023-07-05 10:55:45 +0530

    add form bug fix

--- frontend/js/manager/components/dashboard/allSolutions/AllSolutionForm.jsx
+++ frontend/js/manager/components/dashboard/allSolutions/AllSolutionForm.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import PropTypes, { bool } from "prop-types";
+import PropTypes, { bool, object } from "prop-types";
 import { hashHistory } from "react-router";
 import { findSolutionById, updateSolution, addSolution, findCategories, findSubCategories, findTaxes } from "../../../service/admin_dashboard.js";
 import { getInputEventChangeValue } from "../../../../react_utils.js";
@@ -10,59 +10,51 @@ import Modal from "../../../../components/Modal.jsx";
 import Loading from "../../../../components/Loading.jsx";
 import FormErrorList from "../../../../components/FormErrorList.jsx";
 import JoditEditor from "jodit-react";
-
-const FieldErrors = PropTypes.arrayOf(PropTypes.string);
+import { Paperclip, Cross } from "../../../../components/Icons.jsx";
 
 export default class AllSolutionForm extends React.Component {
     static propTypes = {
         params: PropTypes.shape({
             solutionId: PropTypes.string,
-        }).isRequired,
-        solution: PropTypes.shape({
-            name:PropTypes.string,
-            url_structure:PropTypes.string,
-            price: PropTypes.number,
-            about: PropTypes.string,
-            overview: PropTypes.string,
-            how_it_work: PropTypes.string,
-            short_description:PropTypes.string,
-            execution_time:PropTypes.string,
-            category: PropTypes.shape({
-                id: PropTypes.number.isRequired,
-                name: PropTypes.string.isRequired,
-            }),
-            sub_category: PropTypes.shape({
-                id: PropTypes.number.isRequired,
-                name: PropTypes.string.isRequired,
-            }),
-            tax: PropTypes.shape({
-                id: PropTypes.number.isRequired,
-                rate: PropTypes.number.isRequired,
-            }),
         }),
-        errors: PropTypes.shape({
-			name: FieldErrors,
-			url_structure: FieldErrors,
-			price: FieldErrors,
-			about: FieldErrors,
-			overview: FieldErrors,
-			how_it_work: FieldErrors,
-			short_description: FieldErrors,
-			execution_time: FieldErrors,
-			category: FieldErrors,
-			sub_category: FieldErrors,
-			tax: FieldErrors,
-		}).isRequired,
-
-        
+        category: PropTypes.shape({
+            id: PropTypes.number.isRequired,
+            name: PropTypes.string.isRequired,
+        }),
+        sub_category: PropTypes.shape({
+            id: PropTypes.number.isRequired,
+            name: PropTypes.string.isRequired,
+        }),
+        tax: PropTypes.shape({
+            id: PropTypes.number.isRequired,
+            rate: PropTypes.number.isRequired,
+            name: PropTypes.string.isRequired,
+        }),
     };
-    
+
     state = {
         loading: false,
+        solution: {
+            name: "",
+            url_structure: "",
+            price: 0,
+            // category: "",
+            // sub_category: "",
+            // tax: "",
+            about: "",
+            overview: "",
+            how_it_work: "",
+            execution_time: "",
+            short_description: "",
+        },
         categories: [],
         sub_categories: [],
         taxes: [],
-        errors: {}
+        uploadedFiles: [],
+        fileObjects: [],
+        errors: {
+
+        }
     };
 
     setLoading = (loadingState) => {
@@ -76,11 +68,18 @@ export default class AllSolutionForm extends React.Component {
     componentDidMount() {
         if (this.props.params.solutionId) {
             this.setLoading(true);
-            findSolutionById(this.props.params.solutionId).then((solution) => {
-                this.setState({
-                    solution: Object.assign({}, solution)
-                });
-            }).always(() => this.setLoading(false));
+            findSolutionById(this.props.params.solutionId)
+                .then((solution) => {
+                    this.setState({
+                        solution: {
+                            ...solution,
+                            category: solution.category.id, //for pre-filled values
+                            sub_category: solution.sub_category.id,
+                            tax: solution.tax.id,
+                        },
+                    });
+                })
+                .always(() => this.setLoading(false));
         }
 
         findCategories().then((categories) => {
@@ -88,30 +87,29 @@ export default class AllSolutionForm extends React.Component {
         });
         findSubCategories().then((sub_categories) => {
             this.setState({ sub_categories });
-        })
+        });
         findTaxes().then((taxes) => {
             this.setState({ taxes });
-        })
+        });
     }
 
     fieldChanged = (e) => {
         this.setState({
-            solution: Object.assign({}, this.state, getInputEventChangeValue(e))
+            solution: Object.assign({}, this.state.solution, getInputEventChangeValue(e))
         });
     };
 
     onSubmit = (e) => {
         e.preventDefault();
-        console.log(this.state)
         var promise;
         if (this.props.params.solutionId) {
             promise = updateSolution(
                 this.props.params.solutionId,
-                this.state
+                this.state.solution,
             );
         } else {
             promise = addSolution(
-                this.state
+                this.state.solution,
             );
         }
         promise.then(function () {
@@ -123,104 +121,95 @@ export default class AllSolutionForm extends React.Component {
                 });
             }
         });
+
     };
-
-
 
     handleEditorChange = (editorName, newContent) => {
         this.setState({
             solution: {
-                ...this.state,
+                ...this.state.solution,
                 [editorName]: newContent
             }
         });
     }
-    
+
     render() {
         if (this.state.loading) {
             return (<Loading />);
         }
         var modalTitle = this.props.params.solutionId ? "Edit Solution" : "Add Solution";
-        const { solution, categories, sub_categories, taxes,category,sub_category,tax, loading, errors } = this.state;
-        console.log(this.solution)
+
         return (
             <Modal modalTitle={modalTitle} onClose={hashHistory.goBack}>
                 <form onSubmit={this.onSubmit}>
                     <FormErrorList errors={this.state.errors.non_field_errors} />
                     <div className="row">
                         <div className="col-md-6">
-                            <FormInput label="Solution Name" type="text" value={this.state.name} name="name" onChange={this.fieldChanged} errors={this.state.errors.name} placeholder="Solution Name" />
+                            <FormInput label="Solution Name" type="text" value={this.state.solution.name} name="name" onChange={this.fieldChanged} errors={this.state.errors.name} placeholder="Solution Name" />
                         </div>
                         <div className="col-md-6">
-                            <FormInput label="URL Structure" type="text" value={this.state.url_structure} name="url_structure" onChange={this.fieldChanged} errors={this.state.errors.url_structure} placeholder="URL Structure" />
+                            <FormInput label="URL Structure" type="text" value={this.state.solution.url_structure} name="url_structure" onChange={this.fieldChanged} errors={this.state.errors.url_structure} placeholder="URL Structure" />
                         </div>
                     </div>
                     <div className="row">
                         <div className="col-md-6">
-                        <FormSelect
-                            label="Category"
-                            name="category"
-                            value={category}
-                            onChange={this.fieldChanged}
-                        >
-                            <option value=""></option>
-                            {categories.map(category => (
-                            <option key={category.id} value={category.id}>
-                                {category.name}
-                            </option>
-                            ))}
-                        </FormSelect>
-                          </div>
-                        <div className="col-md-6">
-                            <FormSelect label="Sub Category" name="sub_category"
-                                value={sub_category}
-                                onChange={this.fieldChanged} >
-                                <option value=""></option>
-                                {sub_categories.map(sub_category => (
-                                    <option key={sub_category.id} value={solution.sub_category.id}>
-                                    {solution.sub_category.name}
+                            <FormSelect
+                                label="Category"
+                                name="category"
+                                value={this.state.solution.category}
+                                onChange={this.fieldChanged}
+                            >
+                                <option value="">----------</option>
+                                {this.state.categories.map((category) => (
+                                    <option key={category.id} value={category.id}>
+                                        {category.name}
                                     </option>
-                                ))}
-                              </FormSelect>
-                        </div>
-                    </div>
-                    <div className="row">
-                        <div className="col-md-6">
-                            <FormInput label="Price" type="text" value={this.state.price} name="price" onChange={this.fieldChanged} errors={this.state.errors.price} placeholder="Price" />
-                        </div>
-                        <div className="col-md-6">
-                            <FormSelect label="Tax" name="tax" 
-                            value={tax}
-                            onChange={this.fieldChanged} >
-                            <option value=""></option>
-							{this.state.taxes.map(tax => (
-                                    <option key={tax.id}
-                                        value={tax.id}
-                                
-                                    >{tax.name}</option>
                                 ))}
                             </FormSelect>
                         </div>
+                        <div className="col-md-6">
+                            <FormSelect
+                                label="Sub Category"
+                                name="sub_category"
+                                value={this.state.solution.sub_category}
+                                onChange={this.fieldChanged}
+                            >
+                                <option value="">----------</option>
+                                {this.state.sub_categories.map((sub_category) => (
+                                    <option key={sub_category.id} value={sub_category.id}>
+                                        {sub_category.name}
+                                    </option>
+                                ))}
+                            </FormSelect>
+                        </div>
+
                     </div>
-                    <div className="row" style={{ marginTop: "2rem" }}>
-                        <div className="col-md-12">
-                            <label>About</label>
-                            <JoditEditor
-                                // ref={this.aboutEditor}
-                                name="about"
-                                value={this.state.about}
-                                onChange={(newContent) => this.handleEditorChange('about', newContent)}
-                                errors={this.state.errors.about}
-                            />
+                    <div className="row">
+                        <div className="col-md-6">
+                            <FormInput label="Price" type="text" value={this.state.solution.price} name="price" onChange={this.fieldChanged} errors={this.state.errors.price} placeholder="Price" />
+                        </div>
+                        <div className="col-md-6">
+                            <FormSelect
+                                label="Tax"
+                                name="tax"
+                                value={this.state.solution.tax}
+                                onChange={this.fieldChanged}
+                            >
+                                <option value="">----------</option>
+                                {this.state.taxes.map((tax) => (
+                                    <option key={tax.id} value={tax.id}>
+                                        {tax.name}
+                                    </option>
+                                ))}
+                            </FormSelect>
                         </div>
                     </div>
                     <div className="row" style={{ marginTop: "1rem" }}>
                         <div className="col-md-12">
                             <label>Overview</label>
                             <JoditEditor
-                                // ref={this.overviewEditor}
                                 name="overview"
-                                value={this.state.overview}
+                                value={this.state.solution.overview}
                                 onChange={(newContent) => this.handleEditorChange('overview', newContent)}
                                 errors={this.state.errors.overview}
                             />
@@ -230,9 +219,8 @@ export default class AllSolutionForm extends React.Component {
                         <div className="col-md-12">
                             <label>How it Works</label>
                             <JoditEditor
-                                // ref={this.howItWorkEditor}
                                 name="how_it_work"
-                                value={this.state.how_it_work}
+                                value={this.state.solution.how_it_work}
                                 onChange={(newContent) => this.handleEditorChange('how_it_work', newContent)}
                                 errors={this.state.errors.how_it_work}
                             />
@@ -242,9 +230,8 @@ export default class AllSolutionForm extends React.Component {
                         <div className="col-md-12">
                             <label>Execution Time</label>
                             <JoditEditor
-                                // ref={this.executionTimeEditor}
                                 name="execution_time"
-                                value={this.state.execution_time}
+                                value={this.state.solution.execution_time}
                                 onChange={(newContent) => this.handleEditorChange('execution_time', newContent)}
                                 errors={this.state.errors.execution_time}
                             />
@@ -254,14 +241,25 @@ export default class AllSolutionForm extends React.Component {
                         <div className="col-md-12">
                             <label>Short Description</label>
                             <JoditEditor
-                                // ref={this.shortDescEditor}
                                 name="short_description"
-                                value={this.state.short_description}
+                                value={this.state.solution.short_description}
                                 onChange={(newContent) => this.handleEditorChange('short_description', newContent)}
                                 errors={this.state.errors.short_description}
                             />
                         </div>
                     </div>
+                    <div className="row" style={{ marginTop: "2rem" }}>
+                        <div className="col-md-12">
+                            <label>About</label>
+                            <JoditEditor
+                                name="about"
+                                value={this.state.solution.about}
+                                onChange={(newContent) => this.handleEditorChange('about', newContent)}
+                                errors={this.state.errors.about}
+                            />
+                        </div>
+                    </div>
+
                     <SaveButton />
                 </form>
             </Modal>
