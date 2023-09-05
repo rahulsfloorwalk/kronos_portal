4e3e1d817bf57ee66d08a92c291b6c5d6881c6ac
arpan <arpan.patidar@floorwalk.in>
2023-09-05 13:44:58 +0530

    fix bug

--- manager/viewss/mp_order.py
+++ manager/viewss/mp_order.py
@@ -184,7 +184,7 @@ class MpPaymentView(APIView):
         order_amount = int((order.price + tax_amount) *100)  # Amount in paise (e.g., 1000 paise = Rs. 10)
         order_currency = 'INR'
         order_id=order.id
-        order_receipt = f'order_receipt_{order_id}'
+        order_receipt = 'order_receipt_{}'.format(order_id)
         notes = {'note_key': 'note_value'}
         response = client.order.create(
             {'amount': order_amount, 'currency': order_currency, 'receipt': order_receipt, 'notes': notes}
