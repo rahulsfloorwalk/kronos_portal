d72af055e65b53d4c3af9e46356aad98e6f05ccc
Rahul Solanki <rahul.solanki@floorwalk.in>
2024-05-18 16:34:52 +0530

    Ai insight query optimise and section summary add in report_summary

--- frontend/js/client/service/twitter.js
+++ frontend/js/client/service/twitter.js
@@ -1,9 +1,9 @@
 import $ from "jquery";
 import { url } from "../../../config.js";
 
-// export function fetchClientHandles(){
-// 	return $.get( url.api_base_path + "client/twitter/handles");
-// }
+export function fetchClientHandles(){
+	return $.get( url.api_base_path + "client/twitter/handles");
+}
 
 export function fetchReportSummaryClientHandles(){
 	return $.get( url.api_base_path + "client/report_summary/handles");
@@ -13,9 +13,9 @@ export function fetchClientReportSummaryFeedByHandle(audit_cycle_id){
 	return $.get( url.api_base_path + `client/summary/audit_report/${audit_cycle_id}`);
 }
 
-// export function fetchClientTwitterFeedByHandle(handle_id){
-// 	return $.get( url.api_base_path + `client/twitter/handles/${handle_id}/feed`);
-// }
+export function fetchClientTwitterFeedByHandle(handle_id){
+	return $.get( url.api_base_path + `client/twitter/handles/${handle_id}/feed`);
+}
 
 // use this to get comparison and stats data across twitter handles
 export function fetchClientTwitterStats(){
