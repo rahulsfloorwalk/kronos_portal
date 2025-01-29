a09be383ed945f014f47859021c83337ee4c799a
Rahul Solanki <rahul.solanki@floorwalk.in>
2025-01-29 17:11:44 +0530

    add nps_score in audit_store api

--- client_report/service/audit_section.py
+++ client_report/service/audit_section.py
@@ -265,6 +265,7 @@ def get_audit_store_aggregation_for_client(audit_cycle_id, user_id):
             'store_type': audit_store.audit.store.type,
             'store_priority': audit_store.audit.store.priority,
             'attribute_data': audit_store.attribute_data,
+            'nps_score': audit_store.nps_section,
             'sections': __get_mean_for_report_browser(sections, (audit_store,)),
             'total_score': {
                 'percentage': total_pct,
