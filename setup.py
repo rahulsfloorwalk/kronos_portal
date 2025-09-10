24d5bf7d8afb85d3d9ff38516c750d71f58cac00
Rahul Solanki <rahul.solanki@floorwalk.in>
2025-09-10 18:54:16 +0530

    setup.py upgrade for bump patch

--- setup.py
+++ setup.py
@@ -18,7 +18,8 @@ if __name__ == "__main__":
         sys.exit()
 
     if sys.argv[-2] == 'bump':
-        call(["bumpversion", "--commit", "--tag", "--current-version", version, "--tag-name", "{new_version}", sys.argv[-1], "setup.py", "./frontend/package.json"])
+        # call(["bumpversion", "--commit", "--tag", "--current-version", version, "--tag-name", "{new_version}", sys.argv[-1], "setup.py", "./frontend/package.json"])
+        call([sys.executable, "-m", "bumpversion", "--commit", "--tag","--current-version", version, "--tag-name", "{new_version}",sys.argv[-1], "setup.py", "./frontend/package.json"])
         sys.exit()
 
     if sys.argv[-1] == 'test':
