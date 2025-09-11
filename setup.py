ef1551c779684a6bd96016023e0216f262fc51e1
Rahul Solanki <rahul.solanki@floorwalk.in>
2025-09-11 10:48:29 +0530

    install bump2version

--- setup.py
+++ setup.py
@@ -18,8 +18,9 @@ if __name__ == "__main__":
         sys.exit()
 
     if sys.argv[-2] == 'bump':
+        call(["bump2version", "--commit", "--tag", "--current-version", version, "--tag-name", "{new_version}", sys.argv[-1], "setup.py", "./frontend/package.json"])
         # call(["bumpversion", "--commit", "--tag", "--current-version", version, "--tag-name", "{new_version}", sys.argv[-1], "setup.py", "./frontend/package.json"])
-        call([sys.executable, "-m", "bumpversion", "--commit", "--tag","--current-version", version, "--tag-name", "{new_version}",sys.argv[-1], "setup.py", "./frontend/package.json"])
+        # call([sys.executable, "-m", "bumpversion", "--commit", "--tag","--current-version", version, "--tag-name", "{new_version}",sys.argv[-1], "setup.py", "./frontend/package.json"])
         sys.exit()
 
     if sys.argv[-1] == 'test':
